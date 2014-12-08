582b55520c1ea4d008de0b18ccce84fff10edb27
Lisong Guo <lisong.guo@inria.fr>
2014-12-08 23:35:30 +0100

    reverse a linked list.

--- src/ListNode.java
+++ src/ListNode.java
@@ -12,4 +12,41 @@ public class ListNode {
 		val = x;
 		next = null;
 	}
+
+
+	/**
+	 * Reverse a linked list in place 
+	 * 
+	 * 1 -> 2 -> 3 -> NULL 
+	 * 3 -> 2 -> 1 -> NULL
+	 * 
+	 * @param head
+	 * @return
+	 */
+	public static ListNode reverse(ListNode head){
+		ListNode preIter = head, postIter = null;
+		if(preIter == null)
+			return null;
+		
+		postIter = preIter.next;
+		
+		while(postIter != null){
+			ListNode nextIter = postIter.next;
+			postIter.next = preIter;  // reverse the link
+			
+			// move on
+			preIter = postIter;
+			postIter = nextIter;
+		}
+		
+		head.next = null; // end the list
+		return preIter;
+	}
 }
+
+
+
+
+
+
+
