6b1cd28de4c7f1b65995600a912a8b239283d8b9
Lisong Guo <lisong.guo@inria.fr>
2014-12-11 18:05:53 +0100

    real dynamic programming for MaxisumSubarray.

--- src/MaximumSubarray.java
+++ src/MaximumSubarray.java
@@ -6,6 +6,13 @@
  * which has the largest sum.
 									  	         	
 	For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+           	
+j=1	-2  1 -3  4 -1  2  1 -5 4     A 
+
+j=2	-1 -2  1  3  1  3 -4 -1 
+j=3 -4  2  0  5  2 -2  0 
+j=4 
+	
 		the contiguous subarray [4,−1,2,1] has the largest sum = 6.
  * 
  * @author Lisong Guo<lisong.guo@me.com>
@@ -16,41 +23,24 @@ public class MaximumSubarray {
 
 	
 	/**
-	 * Divide and conquer or dynamic programming ? 
-	 * This algorithm can do more than returning the maximum sum, 
-	 * 	but also returning the subarray that has the maximum sum.  
+	 * dynamic programming.  
 	 */
     public int maxSubArray(int[] A) {
     	if(A.length == 0){
     		return 0;
     	}
     	
-    	if(A.length == 1){
-    		return A[0];
-    	}
-    	
-        int max_sum = Integer.MIN_VALUE;
+    	int [] dp = new int[A.length]; 
         
-        int [] dp_array = new int[A.length]; 
+        int max_sum = A[0];
+        dp[0] = A[0];
     	
-        for(int i=A.length-1; i>=0; i--){
-        	dp_array[i] = A[i];
-            
-        	if(dp_array[i] > max_sum){
-        		max_sum = dp_array[i]; 
-        	}
-        	
-    		for(int j=i-1; j>=0; j--){
-    			dp_array[j] = dp_array[j+1] + A[j];
-    			 
-    			if(dp_array[j] > max_sum){
-    				max_sum = dp_array[j];
-    			}
-    		}
+        for(int i=1; i<A.length; i++){
+            dp[i] = Math.max(dp[i-1] + A[i], A[i]);
+            max_sum = Math.max(dp[i], max_sum);
     	}
         
-        return max_sum;
-    	 
+        return max_sum;	 
     }
     
     
@@ -58,8 +48,8 @@ public class MaximumSubarray {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int [] A = {-2,1,-3,4,-1,2,1,-5,4};
-		//int [] A = {};
+		//int [] A = {-2,1,-3,4,-1,2,1,-5,4};
+		int [] A = {1};
 		MaximumSubarray solution = new MaximumSubarray();
 		
 		System.out.println(solution.maxSubArray(A));
