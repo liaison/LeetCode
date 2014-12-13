bf4ed15cd5cd33f343efb98dc357d79b4da1bf76
Lisong Guo <lisong.guo@inria.fr>
2014-12-14 00:31:43 +0100

    InsertInterval, which is basically a MergeInterval problem.

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -96,20 +96,6 @@ public class MergeIntervals {
     }
     */
 	
-	class IntervalComparator implements Comparator<Interval> {
-	    @Override
-	    public int compare(Interval a, Interval b) {
-	        if(a.start < b.start){
-	        	return -1;
-	        }else if(a.start == b.start){
-	        	return 0;
-	        }else{
-	        	return 1;
-	        }
-	    }
-	}
-
-	
 	public List<Interval> merge(List<Interval> intervals){
 		LinkedList<Interval> result = new LinkedList<Interval>();
 		
@@ -133,6 +119,60 @@ public class MergeIntervals {
 		
 		return result;
 	}
+
+	
+	class IntervalComparator implements Comparator<Interval> {
+	    @Override
+	    public int compare(Interval a, Interval b) {
+	        if(a.start < b.start){
+	        	return -1;
+	        }else if(a.start == b.start){
+	        	return 0;
+	        }else{
+	        	return 1;
+	        }
+	    }
+	}
+
+	/**
+	 * 
+		Given a set of non-overlapping intervals, 
+			insert a new interval into the intervals (merge if necessary).
+
+		You may assume that the intervals were initially sorted according to their start times.
+
+		Example 1:
+			Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+		Example 2:
+			Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+			This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+	 */
+	
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    	intervals.add(newInterval);
+    	LinkedList<Interval> newResult = new LinkedList<Interval>();
+    	
+    	Collections.sort(intervals, new IntervalComparator());
+    	int start = intervals.get(0).start;
+    	int end = intervals.get(0).end;
+    	
+    	for(Interval i : intervals){
+    		if(i.start <= end){
+    			end = Math.max(end, i.end);
+    			continue;
+    		}else{
+    			newResult.add(new Interval(start, end));
+    			start = i.start;
+    			end = i.end;
+    		}
+    	}
+    	
+    	newResult.add(new Interval(start, end));
+    	
+    	return newResult;
+    }
     
     
     private static List<Interval> getIntervalList(int [] input){
@@ -171,6 +211,18 @@ public class MergeIntervals {
 		List<Interval> result = solution.merge(getIntervalList(input));
 
 		Utils.printIntervalList(result);
+		
+		
+		//int input2 [] = {1,3,  6,9};  Interval newInterval = new Interval(2,5);
+		//int input2 [] = {1,2, 3,5, 6,7,	8,10, 12,16};
+		//Interval newInterval = new Interval(4,9);
+		int input2 [] = {1,5};
+		Interval newInterval = new Interval(5, 7);
+		
+		List<Interval> input2_list = getIntervalList(input2);
+		List<Interval> input2_res = 
+				solution.insert(input2_list, newInterval);
+		Utils.printIntervalList(input2_res);
 	}
 
 }
