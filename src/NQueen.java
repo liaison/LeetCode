84f2b8be4acb1af5b5e8e5f64c8fc3aa1438c510
Lisong <lisong.guo@inria.fr>
2014-12-24 19:00:59 +0100

    refacture the code for NQueen.

--- src/NQueen.java
+++ src/NQueen.java
@@ -48,25 +48,31 @@ public class NQueen {
 	//   and the value at the index is the column index. 
 	private int [] colPosition = null;
 	
-	private void addResult(){
-		String [] newSchema = new String[this.N];
-		StringBuffer rowStr = new StringBuffer(this.N);
-		
-		for(int row=0; row<this.N; row++){
-			
-			for(int c=0; c<this.N; c++){
-				if(c == colPosition[row]){
-					rowStr.append('Q');
-				}else{
-					rowStr.append('.');
-				}
-			}
-			
-			newSchema[row] = rowStr.toString();
-			rowStr.delete(0, this.N);
+
+    public List<String[]> solveNQueens(int n) {
+    	this.N = n;
+    	this.stripMap = new boolean[4*this.N];
+    	this.columnMap = new boolean[this.N];
+    	this.colPosition = new int[this.N+1];
+    	
+    	backtrack(0, 0);
+    	return res;
+    }
+    
+	private void backtrack(int curRow, int count){
+		if(count == N){
+			addResult();
+			return;
 		}
 		
-		this.res.add(newSchema);
+		for(int col=0; col<N; col++){
+			if(isSafe(curRow, col)){
+				placeQueue(curRow, col);
+				backtrack(curRow+1, count+1);
+				// remove this candidate, try another one
+				removeQueue(curRow, col);
+			}
+		}
 	}
 	
 	private boolean isSafe(int row, int col){
@@ -102,38 +108,25 @@ public class NQueen {
 		columnMap[col] = false;
 	}
 	
-	private void backtrack(int curRow, int count){
-		if(count == N){
-			addResult();
-			return;
-		}
+	private void addResult(){
+		String [] newSchema = new String[this.N];
+		StringBuffer rowStr = new StringBuffer(this.N);
 		
-		for(int col=0; col<N; col++){
-			if(isSafe(curRow, col)){
-				
-				placeQueue(curRow, col);
-					
-				backtrack(curRow+1, count+1);
-				
-				// remove this candidate, try another one
-				removeQueue(curRow, col);
+		for(int row=0; row<this.N; row++){		
+			for(int c=0; c<this.N; c++){
+				if(c == colPosition[row]){
+					rowStr.append('Q');
+				}else{
+					rowStr.append('.');
+				}
 			}
+			newSchema[row] = rowStr.toString();
+			rowStr.delete(0, this.N);
 		}
+		this.res.add(newSchema);
 	}
 	
-    public List<String[]> solveNQueens(int n) {
-    	this.N = n;
-    	this.stripMap = new boolean[4*this.N];
-    	this.columnMap = new boolean[this.N];
-    	this.colPosition = new int[this.N+1];
-    	
-    	//Arrays.fill(this.stripMap, false);
-    	
-    	backtrack(0, 0);
-    	return res;
-    }
-    
-    
+	
 	public static void main(String[] args) {
 		NQueen solution = new NQueen();
 		
