9e7c898e3cbeb9444cde067bffdd5794f3a492ce
Lisong Guo <lisong.guo@inria.fr>
2014-12-30 00:58:36 +0100

    Horspool algorithm for the string search algorithm, a variant of Boyer-Moore algorithm.

--- src/StrStr.java
+++ src/StrStr.java
@@ -1,3 +1,5 @@
+import java.util.Hashtable;
+
 
 
 /**
@@ -13,6 +15,7 @@
  */
 public class StrStr {
 
+	/**
     public int strStr(String haystack, String needle) {
     	int nl = needle.length();
     	int hl = haystack.length();
@@ -26,9 +29,55 @@ public class StrStr {
     	
     	return -1;
     }
+    */
+    
+    private Hashtable<Character, Integer> bcTable;
+    
+    private void preBadChar(String p){
+    	bcTable = new Hashtable<Character, Integer>();
+    	
+    	int s = p.length();
+    	for(int i=0; i<s-1; ++i){
+    		bcTable.put(p.charAt(i), s-i-1);
+    	}
+    }
 	
+    public int strStr(String haystack, String needle) {
+    	this.preBadChar(needle);
+    	
+    	int hl = haystack.length();
+    	int nl = needle.length();
+    	int i = 0;
+    	while(i <= hl-nl){
+    		int j = nl-1;
+    		while(j >= 0){
+    			if(haystack.charAt(i+j) != needle.charAt(j)){
+    				break;
+    			}
+    			--j;
+    		}
+    		
+    		if(j < 0){
+    			// find match
+    			return i;
+    		}
+    		
+    		// Not a match, shift to another position
+    		Integer shift = bcTable.get(haystack.charAt(i+nl-1));
+    		if(shift == null){
+    			// really bad character
+    			i += nl;
+    		}else{
+    			i += shift;
+    		}
+    	}
+    	
+    	return -1;
+    }
+    
+    
 	public static void main(String[] args) {
-		String haystack = "thisisaneedle";
+		String haystack = "thisisaneedleinahaystack";
 		String needle = "needle";
 		StrStr solution = new StrStr();
 		
