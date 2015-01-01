b3f98b1c99ab51fe3cf3710e5a0f09088395d12e
Lisong Guo <lisong.guo@inria.fr>
2015-01-01 17:53:59 +0100

    make the code for LongestCommonPrefix more concise.

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -14,23 +14,19 @@ public class LongestCommonPrefix {
     		return "";
     	}
     	
+    	int minLen = Integer.MAX_VALUE;
+    	for(String str : strs){
+    		minLen = Math.min(minLen, str.length());
+    	}
+    	
     	boolean isFound = false;
     	int pc = 0;
-    	while(! isFound){
+    	
+    	while(! isFound && pc < minLen){
     		
-    		int c = -1;
-    		int i = 0;
+    		int c = strs[0].charAt(pc);
     		
-    		for(; i<strs.length; ++i){
-    			
-    			if(pc >= strs[i].length()){
-        			isFound = true;
-    				break;
-        		}
-        		
-    			if(c == -1){
-    				c = strs[i].charAt(pc);	
-    			}
+    		for(int i=1; i<strs.length; ++i){
     			
     			if(c != strs[i].charAt(pc)){
     				isFound = true;
@@ -41,12 +37,14 @@ public class LongestCommonPrefix {
     		++pc;
     	}
     	
-    	return strs[0].substring(0, pc-1);
+    	return strs[0].substring(0, isFound ? pc-1 : pc);
     }
     
     public static void main(String[] args) {
-    	//String [] strs = {"ABC", "A", "ABC"};
-    	String [] strs = {};
+    	//String [] strs = {"ABC", "ABCD", "ABC"};
+    	String [] strs = {"a", "b"};
+    	
+    	//String [] strs = {};
     	
     	LongestCommonPrefix solution = new LongestCommonPrefix();
     	System.out.println(solution.longestCommonPrefix(strs));
