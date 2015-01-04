4554e90b910260f136928f26fabb9bcd80740cf0
Lisong Guo <lisong.guo@inria.fr>
2015-01-04 16:38:38 +0100

    Problem 65. A recursive solution to check weather two trees are the same.

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -81,7 +81,7 @@ return its bottom-up level order traversal as:
 	 */
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         
-    	List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	List<List<Integer>> result = new LinkedList<List<Integer>>();
         
         if(root == null){
         	return result;
@@ -182,6 +182,28 @@ return its bottom-up level order traversal as:
     		}
     	}
     	return true;
+    }
+    
+    /**
+     * Given two binary trees, write a function to check if they are equal or not.
+		Two binary trees are considered equal if they are structurally identical and 
+		the nodes have the same value.
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+    	if(p == null && q == null){
+    		return true;
+    	
+    	}else if(p == null && q != null  ||
+    			 p != null && q == null){
+    		return false;
+    	}else{
+    		// neither p nor q is NULL, check both left and right subtree
+        	
+    		if(p.val != q.val){
+    			return false;
+    		}
+        	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    	}	
     }
     
     
