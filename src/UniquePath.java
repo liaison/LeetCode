56e9d1915003d208c63321eb8b7a9d3e49469864
Lisong Guo <lisong.guo@inria.fr>
2015-01-04 02:23:22 +0100

    Problem 63. Unique path II
    grid with obstacles

--- src/UniquePath.java
+++ src/UniquePath.java
@@ -15,6 +15,16 @@
  */
 public class UniquePath {
 
+	/**
+	 * The result is basically Yang Hui's triangle, 
+	 * 		which is also applied in binomial combination. 
+	
+	public int uniquePaths(int m, int n) {
+		
+	}
+	 */
+	
+    
     public int uniquePaths(int m, int n) {
     	if(m == 0 || n == 0){
     		return 0;
@@ -38,6 +48,43 @@ public class UniquePath {
     }
     
     
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    	if(obstacleGrid == null){
+    		return 0;
+    	}
+    	
+    	int m = obstacleGrid.length;
+    	int n = obstacleGrid[0].length;
+    	
+    	int [][] dp = new int[m][n];
+    	
+    	for(int i=0; i<m; i++){
+    		for(int j=0; j<n; j++){
+    			if(obstacleGrid[i][j] == 1){
+    				// the path is blocked, so no path
+    				dp[i][j] = 0;
+    				continue;
+    			}
+    			
+    			if(i == 0 && j == 0){
+    				dp[0][0] = 1;
+    			
+    			}else if(i == 0){ // j != 0 
+    				dp[i][j] = dp[i][j-1];
+    			
+    			}else if(j == 0){ // i != 0
+    				dp[i][j] = dp[i-1][j];
+    				
+    			}else{ // i != 0 && j != 0
+    				dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    			}
+    		}
+    	}
+    	
+    	return dp[m-1][n-1];
+    }
+	
+    
 	/**
 	 * @param args
 	 */
@@ -45,6 +92,19 @@ public class UniquePath {
 
 		UniquePath solution = new UniquePath();
 		System.out.println(solution.uniquePaths(3,  2));
+		
+		int [][] obstacleGrid = {{0,0,0},
+		                         {0,1,0},
+		                         {0,0,0}};
+		
+		System.out.println(solution.uniquePathsWithObstacles(obstacleGrid));	
 	}
 
 }
+
+
+
+
+
+
+
