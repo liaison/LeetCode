0bc81e0a22eb4b3951d19aa42966496036a0f425
Lisong <lisong.guo@inria.fr>
2015-01-05 17:06:47 +0100

    Problem 68. Maximal Rectangle.

---
+++ src/MaximalRectangle.java
@@ -0,0 +1,89 @@
+
+/**
+ * 
+ * @author Lisong Guo <lisong.guo@me.com>
+ * @date   Jan 05, 2015
+ *
+ */
+public class MaximalRectangle {
+
+	private int maxArea(char[][] matrix, int i, int j){
+		int row_len = matrix.length;
+		int col_len = matrix[0].length;
+		
+		int area = 1; // starting point	
+		for(int ma=i; ma<row_len && col_len > j; ++ma){
+			int na = j;
+			for(; na<col_len; ++na){
+				if(matrix[ma][na] == '0'){
+					//update the boundary of column
+					col_len = na;
+					break;
+				}
+			}
+			
+			// check the area of each rectangle
+			area = Math.max(area, (ma-i+1) * (na-j));
+		}
+		return area;
+	}
+	
+    public int maximalRectangle(char[][] matrix) {
+    	int max_area = 0;
+    	
+    	for(int i=0; i<matrix.length; ++i){
+    		for(int j=0; j<matrix[0].length; ++j){
+    			if(matrix[i][j] == '1'){
+    				// max rectange starting from this point
+    				//  expanding to the right and bottom. 
+    				max_area = Math.max(max_area, this.maxArea(matrix, i, j));
+    			}
+    		}
+    	}
+    	
+    	return max_area;
+    }
+    
+    
+	public static void main(String[] args) {
+		
+		/**
+		
+		char [][] matrix = {
+				{'1', '1', '0', '0', '0'},
+				{'1', '1', '1', '0', '0'},
+				{'0', '1', '1', '0', '0'},
+				{'0', '1', '1', '1', '1'},
+		}; // expect 6;
+		*/
+		
+		
+		//char [][] matrix = {"10","01","01","01","11","00","01"};
+		
+		String [] input = {
+				 "11111111",
+				 "11111110",
+				 "11111110",
+				 "11111000",
+				 "01111000"
+		}; // expect 21
+		
+		char [][] matrix = Utils.string2matrix(input);
+		
+		
+		
+		MaximalRectangle solution = new MaximalRectangle();
+		System.out.println(solution.maximalRectangle(matrix));
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
