98bc3959d1dbf1e9104f992873305f6d7b1203b3
Lisong Guo <lisong.guo@inria.fr>
2015-01-19 19:44:15 +0100

    An O(n) time complexity but O(1) space complexity algorithm for the random list copy problem.
    The solution iterates the list in 3 rounds, but apparently its performance is better than the heap map solution which iterates the list in 2 rounds but consumes more space.

--- src/CopyRandomList.java
+++ src/CopyRandomList.java
@@ -11,7 +11,9 @@ import java.util.Hashtable;
 
 public class CopyRandomList {
 
-	
+	/**
+	 * Time complexity O(N). Space complexity O(N).
+	 
     public RandomListNode copyRandomList(RandomListNode head) {
     	Hashtable<Integer, RandomListNode> nodeTable = 
     			new Hashtable<Integer, RandomListNode>();
@@ -19,6 +21,7 @@ public class CopyRandomList {
     	RandomListNode pseudoHead = new RandomListNode(0);
     	RandomListNode iter = head, p = pseudoHead;
     	
+    	// First round: copy all the nodes without assigning random pointers.
     	while(iter != null){
     		RandomListNode newNode = new RandomListNode(iter.label);
     		nodeTable.put(iter.label, newNode);
@@ -28,7 +31,7 @@ public class CopyRandomList {
     		iter = iter.next;
     	}
     	
-    	// second round: assign the random pointers
+    	// Second round: assign the random pointers
     	iter = head;
     	while(iter != null){
     		if(iter.random != null){
@@ -40,7 +43,62 @@ public class CopyRandomList {
     	
     	return pseudoHead.next;
     }
-	
+	*/
+    
+	/**
+	 * Solution from the code handbook.
+	 *  Associate naturally the original node and copy node in a single linked list.
+	 *    Therefore, we don't need extra space to keep track of the nodes.
+	 *    
+	 * Time complexity O(N) (3 rounds of iteration), space complexity O(1).
+	 */
+    public RandomListNode copyRandomList(RandomListNode head) {
+    	RandomListNode iter = head, next;
+    	
+    	// First round: make copy of each node, 
+    	//		and link them together side-by-side in a single list. 
+    	while(iter != null){
+    		next = iter.next;
+    		
+    		RandomListNode copy = new RandomListNode(iter.label);
+    		iter.next = copy;
+    		copy.next = next;
+    		
+    		iter = next;
+    	}
+    	
+    	// Second round: assign random pointers for the copy nodes.
+    	iter = head;
+    	while(iter != null){
+    		if(iter.random != null){
+    			iter.next.random = iter.random.next;
+    		}
+    		iter = iter.next.next;
+    	}
+    	
+    	// Third round: restore the original list, and extract the copy list.
+    	iter = head;
+    	RandomListNode pseudoHead = new RandomListNode(0);
+    	RandomListNode copy, copyIter = pseudoHead;
+    	
+    	while(iter != null){
+    		next = iter.next.next;
+    		
+    		// extract the copy
+    		copy = iter.next;
+    		copyIter.next = copy;
+    		copyIter = copy;
+    	
+    		// restore the original list
+    		iter.next = next;
+    		
+    		iter = next;
+    	}
+    	
+    	return pseudoHead.next;
+    }
+    
+    
 	public static void main(String[] args) {
 	
 	}
