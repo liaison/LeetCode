a679697d0f30c3122822ca1c53e0444ccf757dda
Lisong G <lisong.guo@icloud.com>
2016-03-01 23:20:51 +0100

    remove tabs

--- src/Anagram.java
+++ src/Anagram.java
@@ -15,10 +15,10 @@ import java.util.List;
  */
 public class Anagram {
 
-	/**
-	 * Given an array of strings, return all groups of strings that are anagrams.
-	   Note: All inputs will be in lower-case.
-	 */
+   /** 
+    * Given an array of strings, return all groups of strings that are anagrams.
+    *  Note: All inputs will be in lower-case.
+    */
     public List<String> anagrams(String[] strs) {
         
     	HashMap<String, ArrayList<Integer>> hashmap = 
@@ -27,44 +27,42 @@ public class Anagram {
 
         // build a hash table, with the "unique" string form as the key
         for(int i=0; i<strs.length; ++i){
-        	char [] charArray = strs[i].toCharArray();
-        	Arrays.sort(charArray);
-        	String key = String.valueOf(charArray);
+            char [] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String key = String.valueOf(charArray);
         	
-    		ArrayList<Integer> value = hashmap.get(key);
-        	if(value == null){
-        		value = new ArrayList<Integer>();
-        		value.add(i);
-        		hashmap.put(key, value);
-        	}else{
-        		value.add(i);
-        	}
+            ArrayList<Integer> value = hashmap.get(key);
+            if(value == null){
+                value = new ArrayList<Integer>();
+                value.add(i);
+                hashmap.put(key, value);
+            }else{
+        	value.add(i);
+            }
         }
-        
+
         // extract anagrams from the hash table.
         Iterator<String> iter = hashmap.keySet().iterator();
         while(iter.hasNext()){
-        	ArrayList<Integer> indexList = 
-        			hashmap.get(iter.next());
-        	if(indexList.size() > 1){
-        		for(Integer i : indexList){
-            		res.add(strs[i]);
-            	}
-        	}
+            ArrayList<Integer> indexList = hashmap.get(iter.next());
+            if(indexList.size() > 1){
+                for(Integer i : indexList){
+            	    res.add(strs[i]);
+                }
+            }
         }
         
         return res;
     }
-    
-    
-	public static void main(String[] args) {
-		String [] strs = {"tim", "mit", "abc", "c"};
 
-		Anagram solution = new Anagram();
-		List<String> res = solution.anagrams(strs);
-		for(int i=0; i<res.size(); ++i){
-			System.out.println(res.get(i));
-		}
-	}
+
+    public static void main(String[] args) {
+        String [] strs = {"tim", "mit", "abc", "c"};
+        Anagram solution = new Anagram();
+        List<String> res = solution.anagrams(strs);
+        for(int i=0; i<res.size(); ++i){
+            System.out.println(res.get(i));
+        }
+    }
 
 }
