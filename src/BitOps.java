62352a31a98173c206b79a4c0c5b2914a13d9373
Lisong G <lisong.guo@icloud.com>
2016-03-07 22:10:29 +0100

    sqrt function

--- src/BitOps.java
+++ src/BitOps.java
@@ -43,51 +43,51 @@ public class BitOps {
         }
 
         /**
-         *      Implement int sqrt(int x).
-         *      Compute and return the square root of x.
-     *  Note: the tag of this problem suggests "binary search".
+         * Implement int sqrt(int x) i.e compute and return the square root of x.
          *
          * This solution would exceed the time limit.
          */
         public int sqrt(int x) {
-                int mid = 0;
-                int low = 0, high = x;
+            int mid = 0;
+            int low = 0, high = x;
 
-                while(low < high - 1){
-                        mid = (low + high) >> 1;
-                        int mid_power = mid * mid;
-                        if(mid_power < x){
-                                low = mid;
-                        }else if(mid_power > x){
-                                high = mid;
-                        }else{
-                                return mid;
-                        }
+            while(low < high - 1){
+                mid = (low + high) >> 1;
+                int mid_power = mid * mid;
+                if(mid_power < x){
+                    low = mid;
+                }else if(mid_power > x){
+                    high = mid;
+                }else{
+                    return mid;
                 }
-                // approximation
-                return low;
-    }
+            }
+            // approximation
+            return low;
+        }
 
         /**
-         * The time complexity of this solution is O(1), i.e. 15 times of iteration.
+         * The time complexity of this solution is O(1),
+         *   i.e. 15 times of iteration.
          * https://oj.leetcode.com/discuss/8897/share-my-o-log-n-solution-using-bit-manipulation
          */
         public int bsqrt(int x){
-                long ans = 0;         // long is necessary, due to overflow of ans * ans
-                long bit = 1l << 16;  // 1L one in long.
+            long ans = 0;         // long is necessary, due to overflow of ans * ans
+            long bit = 1l << 16;  // 1L: one in long.
 
-                while(bit > 0){
-                    // OR operator, set the specific bit to one and keep the rest.
-                        ans |= bit;
-                        if(ans * ans > x){
-                                // XOR operator, revert the current bit from 1 to 0,
-                                //       and preserve the high bits in the result.
-                                ans ^= bit;
-                        }
-                        bit >>= 1;
+            // Deduce the bits of result one by one.
+            while(bit > 0){
+                // OR operator, set the specific bit to one and keep the rest.
+                ans |= bit;
+                if(ans * ans > x){
+                    // XOR operator, revert the current bit from 1 to 0,
+                    //   and preserve the high bits in the result.
+                    ans ^= bit;
                 }
+                bit >>= 1;
+            }
 
-                return (int)ans;
+            return (int)ans;
         }
 
         @Test
