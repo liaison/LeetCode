20b21dad91102c45c98e905b2c7d860a676f23a2
Lisong Guo <lisong.guo@me.com>
2018-07-30 16:02:51 +0200

    refine the testing cases

--- python/Search.py
+++ python/Search.py
@@ -4,6 +4,8 @@ class Solution:
         :type nums: List[int]
         :type target: int
         :rtype: int
+
+            recursive binary search in all sub-trees
         """
         size = len(nums)
         mid = int(size/2)
@@ -14,10 +16,12 @@ class Solution:
         if (nums[mid] == target):
             return mid
         else:
+            # check the left subtree
             left_ret = self.search(nums[0:mid], target)
             if (left_ret != -1):
                 return left_ret
             else:
+                # if not found solution in left subtree, check right subtree
                 right_ret = self.search(nums[mid+1:], target)
                 if (right_ret != -1):
                     return mid + 1 + right_ret
@@ -44,13 +48,12 @@ class Solution:
         return -1
 
 
-def verify(case_name, test_input, test_target):
+def verify(case_name, test_input, test_target, test_func):
     """
        utility function for unit testing
     """
-    solution = Solution()
     print(case_name, test_input, ' target:', test_target)
-    assert(test_target == solution.search(*test_input))
+    assert(test_target == test_func(*test_input))
 
 
 if __name__ == "__main__":
@@ -59,18 +62,19 @@ if __name__ == "__main__":
 
     test_case_1_input = ([4,5,6,7,0,1,2], 4)
     test_case_1_target = 0
-    verify('test case 1:', test_case_1_input, test_case_1_target)
-
+    verify('test case 1:',
+           test_case_1_input, test_case_1_target, solution.search)
 
     test_case_2_input = ([4,5,6,7,0,1,2], 3)
     test_case_2_target = -1
-    verify('test case 2:', test_case_2_input, test_case_2_target)
+    verify('test case 2:',
+           test_case_2_input, test_case_2_target, solution.search)
     
-
     sorted_list_1 = [x for x in range(5)]
-    target = 2
-    print(sorted_list_1, target)
-    print(solution.binary_search(sorted_list_1, target))
+    test_case_3_input = (sorted_list_1, 2)
+    test_case_3_target = 2
+    verify('test case 1 for binary search:',
+           test_case_3_input, test_case_3_target, solution.binary_search)
        
 
 
