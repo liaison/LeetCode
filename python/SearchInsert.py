a064c842bff342701141671cbe8cace908bf8b82
Lisong Guo <lisong.guo@me.com>
2018-07-30 14:41:25 +0200

    add a tail-recursive version of the SearchInsert, which is faster than 99.3% of solutions

--- python/SearchInsert.py
+++ python/SearchInsert.py
@@ -10,7 +10,7 @@ You may assume no duplicates in the array.
 
 """
 class Solution:
-    def searchInsert(self, nums, target):
+    def _searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -31,6 +31,35 @@ class Solution:
             return self.searchInsert(nums[0:mid_index], target)
         else:
             return mid_index + 1 + self.searchInsert(nums[mid_index+1:], target)
+
+
+    def searchInsert(self, nums, target):
+        return self.tail_recursive_searchInsert(0, nums, target)
+
+
+    def tail_recursive_searchInsert(self, start, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+
+          return the position to insert a new element into the sorted list.
+          Note: the solution can be implemented in a recursive way.
+        """
+        size = len(nums)
+        if (size == 0):
+            # empty input list, early exit
+            return start
+
+        mid_index = int(size/2)
+        if (nums[mid_index] == target):
+            return start + mid_index 
+        elif (nums[mid_index] > target):
+            return self.tail_recursive_searchInsert(
+                            start, nums[0:mid_index], target)
+        else:
+            return self.tail_recursive_searchInsert(
+                            start+mid_index+1, nums[mid_index+1:], target)
 
 
 def verify(case_name, test_input, test_target):
