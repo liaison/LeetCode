5ac9a03a13d7545c6488b5810c342baa7f9c6316
Lisong Guo <lisong.guo@me.com>
2018-07-31 15:00:36 +0200

    problem description for the findPeakElement

--- c++/findPeakElement.cpp
+++ c++/findPeakElement.cpp
@@ -1,3 +1,22 @@
+/**
+
+A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], 
+find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index
+to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+@author: Lisong Guo <lisong.guo@me.com
+@date:   July 31, 2018
+
+*/
+
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -5,7 +24,13 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        
         int low = 0, high = nums.size()-1;
+        
+        if(high == -1){
+            // empty list
+            return -1;
+        }
 
         while(low < high){
             int mid = (low + high)/2;
