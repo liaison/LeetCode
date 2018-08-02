8e6ed2eb15667db80d25ef2981375b86330dbaee
Lisong Guo <lisong.guo@me.com>
2018-08-02 21:12:49 +0200

    comment

--- python/searchMatrix.py
+++ python/searchMatrix.py
@@ -1,4 +1,15 @@
+"""
 
+Write an efficient algorithm that searches for a value in an m x n matrix.
+This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+@author: Lisong Guo <lisong.guo@me.com>
+@date:   Aug 02, 2018
+
+"""
 
 class Solution:
     def searchMatrix(self, matrix, target):
