8cdd1f06b0f127ea49a8e0081d51d7e1f5dfc442
Lisong Guo <lisong.guo@me.com>
2018-08-03 20:16:00 +0200

    problem description for minPathSum

--- python/minPathSum.py
+++ python/minPathSum.py
@@ -1,4 +1,25 @@
+"""
 
+Given a m x n grid filled with non-negative numbers, find a path from top left 
+to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+    Input:
+    [
+        [1,3,1],
+        [1,5,1],
+        [4,2,1]
+    ]
+    Output: 7
+    Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ 
+@author: Lisong Guo <lisong.guo@me.com
+@date:   Aug 03, 2018
+
+"""
 
 class Solution:
     def minPathSum(self, grid):
