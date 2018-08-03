826125b5f6bc527053fcaed9fd0dfa803775a86c
Lisong Guo <lisong.guo@me.com>
2018-08-03 20:56:23 +0200

    using 2D array instead of 1D to have better memory locality ? for the dp solution

--- python/minPathSum.py
+++ python/minPathSum.py
@@ -22,7 +22,7 @@ Example:
 """
 
 class Solution:
-    def minPathSum(self, grid):
+    def _minPathSum(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
@@ -42,12 +42,35 @@ class Solution:
         while(i >= 0):
             i_row = i // n_col
             i_col = i % n_col
-            min_path = min(dp[(i_row+1)*n_col + i_col],
-                           dp[i_row*n_col + i_col + 1])
+            min_path = min(dp[i + n_col], dp[i + 1])
             dp[i] = grid[i_row][i_col] + min_path
             i -= 1
 
         return dp[0]
+
+
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n_row = len(grid)
+        n_col = len(grid[0])
+        # extend the dp matrix with pseudo boundary to eliminate the switch cases
+        dp = [[sys.maxsize for _ in range(n_col + 1)] for _ in range(n_row + 1)]
+        dp[n_row-1][n_col] = 0
+
+        for i_row in range(n_row-1, -1, -1):
+            for i_col in range(n_col-1, -1, -1):
+                min_path = min(dp[i_row+1][i_col], dp[i_row][i_col+1])
+                dp[i_row][i_col] = grid[i_row][i_col] + min_path
+        
+        return dp[0][0]
+
+
+def print_matrix(m):
+    for i in range(len(m)):
+        print(m[i])
 
 
 def verify(case_name, test_input, test_target, test_func):
@@ -59,6 +82,8 @@ def verify(case_name, test_input, test_target, test_func):
           ' output:', actual_output)
     assert(test_target == actual_output)
 
+
+import sys
 
 if __name__ == "__main__":
 
