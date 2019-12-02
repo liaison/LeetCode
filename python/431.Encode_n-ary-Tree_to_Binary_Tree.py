e0316eac5582bc1eccff4c203895c85a2dc2f5f2
Lisong Guo <lisong.guo@me.com>
2019-12-02 23:40:50 +0100

    431. encode n-ary tree to binary tree

---
+++ python/431.Encode_n-ary-Tree_to_Binary_Tree.py
@@ -0,0 +1,90 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+"""
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+"""
+
+"""
+
+solution reference:
+    https://en.wikipedia.org/wiki/M-ary_tree#Convert_a_m-ary_tree_to_binary_tree
+
+"""
+
+from collections import deque
+
+class Codec:
+
+    def encode(self, root):
+        """Encodes an n-ary tree to a binary tree.
+        :type root: Node
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+
+        rootNode = TreeNode(root.val)
+        queue = deque([(rootNode, root)])
+
+        while queue:
+            parent, curr = queue.popleft()
+                        
+            prevBNode = None
+            headBNode = None
+            for child in curr.children:
+                newBNode = TreeNode(child.val)
+                if prevBNode:
+                    prevBNode.right = newBNode
+                else:
+                    headBNode = newBNode
+                prevBNode = newBNode
+                queue.append((newBNode, child))
+            
+            parent.left = headBNode
+        
+        return rootNode
+
+
+    def decode(self, data):
+        """Decodes your binary tree to an n-ary tree.
+        :type data: TreeNode
+        :rtype: Node
+        """
+        if not data:
+            return None
+        
+        # should set the default value to [] rather than None,
+        # otherwise it wont pass the test cases.
+        rootNode = Node(data.val, [])
+        
+        queue = deque([(rootNode, data)])
+        
+        while queue:
+            parent, curr = queue.popleft()
+                        
+            firstChild = curr.left
+            sibling = firstChild
+
+            while sibling:
+                #if not parent.children:
+                #    parent.children = []
+                newNode = Node(sibling.val, [])
+                parent.children.append(newNode)
+                queue.append((newNode, sibling))                
+                sibling = sibling.right
+        
+        return rootNode
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(root))
