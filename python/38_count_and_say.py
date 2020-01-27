9281081d4d3c530272c8847ccb060cc7f9e821cf
Lisong Guo <lisong.guo@me.com>
2020-01-27 23:06:53 +0100

    38. count and say. recursion

---
+++ python/38_count_and_say.py
@@ -0,0 +1,45 @@
+"""
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+"""
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        prevNum = ['1', 'E']
+        return ''.join(self.nextNumber(n, prevNum))
+
+    def nextNumber(self, n, prevNum):
+        if n == 1:
+            return prevNum[:-1]
+        
+        nextNum = []
+        prevDigit = prevNum[0]
+        digitCnt = 1
+        for digit in prevNum[1:]:
+            if digit == prevDigit:
+                digitCnt += 1
+            else:
+                nextNum.extend([str(digitCnt), prevDigit])
+                prevDigit = digit
+                digitCnt = 1
+        
+        nextNum.append('E')
+        
+        return self.nextNumber(n-1, nextNum)
