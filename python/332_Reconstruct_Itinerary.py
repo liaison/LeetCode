78ea4fc2fbb9516ac9190b89e0e064de8a09d948
Lisong Guo <lisong.guo@me.com>
2020-02-12 23:01:31 +0100

    332. mark visits with bitmap

--- python/332_Reconstruct_Itinerary.py
+++ python/332_Reconstruct_Itinerary.py
@@ -16,6 +16,8 @@ e.g.  ['TIA', 'JFK'], ['TIA', 'JFK'], ['JFK', 'TIA'], ['JFK', 'TIA']
 class Solution(object):
     def findItinerary(self, tickets):
         """
+            faster than 10% of submissions
+            
         :type tickets: List[List[str]]
         :rtype: List[str]
         """
@@ -57,4 +59,54 @@ class Solution(object):
                     return True
             
         return False
+
+    
+    
+class Solution_Faster(object):
+    """ faster than 19.5 of submissions """
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        from collections import defaultdict
+        self.originMap = defaultdict(list)
+        
+        for ticket in tickets:
+            origin, dest = ticket[0], ticket[1]
+            self.originMap[origin].append(dest)
+        
+        self.visitBitmap = {}
+        
+        # sort the itinerary based on the lexical order
+        for origin, itinerary in self.originMap.items():
+        # Note that we could have multiple identical flights, i.e. same origin and destination.
+            itinerary.sort()
+            self.visitBitmap[origin] = [False]*len(itinerary)
+        
+        self.flights = len(tickets)
+        self.result = []
+        route = ['JFK']
+        self.backtracking('JFK', route)
+        
+        return self.result
+
+
+    def backtracking(self, origin, route):
+        if len(route) == self.flights + 1:
+            self.result = route
+            return True
+        
+        for i, nextDest in enumerate(self.originMap[origin]):
+            # unique label to identify an itinerary
+            if not self.visitBitmap[origin][i]:
+                self.visitBitmap[origin][i] = True
+                ret = self.backtracking(nextDest, route + [nextDest])
+                self.visitBitmap[origin][i] = False
+                if ret:
+                    return True
+            
+        return False
+        
+        
  
