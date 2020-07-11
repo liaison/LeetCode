e769a41a337fa929e2e91eb31497957b55910197
Lisong Guo <lisong.guo@me.com>
2020-07-11 19:10:02 +0200

    952. refactor the implementation of DisjointSetUnion

--- python/952_Largest_Component_Size_By_Common_Factor.py
+++ python/952_Largest_Component_Size_By_Common_Factor.py
@@ -35,20 +35,24 @@ class SolutionTLE:
         return max_size
                 
                 
-                
 class DisjointSetUnion(object):
-    
+
     def __init__(self, size):
         # initially, each node is an independent component
         self.parent = [i for i in range(size+1)]
-        self.rank = [0] * (size+1)
+        # use the size of the component as the rank value
+        self.rank = [1] * (size+1)
     
     def find(self, x):
+        """ return the component id that the element x belongs to. """
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
     
     def union(self, x, y):
+        """ merge the two components that x, y belongs to respectively,
+              and return the merged component id as the result.
+        """
         px, py = self.find(x), self.find(y)
         
         # the two nodes share the same set
@@ -56,21 +60,20 @@ class DisjointSetUnion(object):
             return px
         
         # otherwise, connect the two sets (components)
-        if self.rank[px] < self.rank[py]:
+        if self.rank[px] > self.rank[py]:
             # add the node to the union with less rank (less members)
-            self.parent[py] = px
-            return px
-        elif self.rank[px] > self.rank[py]:
-            self.parent[px] = py
-            return py
-        else:
-            # equal number of member
-            self.parent[py] = px
-            self.rank[px] += 1
-            return px
-                
-                
-                
+            # keeping px as the index of the smaller component
+            px, py = py, px
+        # add the smaller component to the larger one
+        self.parent[px] = py
+        self.rank[py] += self.rank[px]
+        # return the final (merged) group
+        return py
+
+    def rank(self, x):
+        """ return the size of the component that x belongs to"""
+        return self.rank[self.find(x)]
+
                 
                 
                 
