03b07324bc9b6b9de6663641cabfe774777892e9
Lisong Guo <lisong.guo@me.com>
2020-07-21 01:05:15 +0200

    optimized MinMax algorithm

--- python/375_Guess_Higher_or_Lower_Number_II.py
+++ python/375_Guess_Higher_or_Lower_Number_II.py
@@ -17,3 +17,23 @@ class Solution:
             return min_cost
         
         return cost(0, n)
+
+
+class SolutionOptimized:
+    def getMoneyAmount(self, n: int) -> int:
+        
+        @lru_cache(maxsize=None)
+        def cost(low, high):
+            if low >= high:
+                return 0
+            
+            min_cost = float('inf')
+            # only consider using the right half of the elments as pivots,
+            #   as they are the worst scenarios.
+            for pivot in range((low+high)//2, high):
+                worst_scenario = pivot + max(cost(low, pivot-1), cost(pivot+1, high))
+                min_cost = min(min_cost, worst_scenario)
+            
+            return min_cost
+        
+        return cost(1, n)
