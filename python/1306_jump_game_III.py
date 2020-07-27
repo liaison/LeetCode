aa32a1a82d2e51f639f855a765c375730ecece99
Lisong Guo <lisong.guo@me.com>
2020-07-27 19:01:50 +0200

    1306. backtracking

--- python/1306_jump_game_III.py
+++ python/1306_jump_game_III.py
@@ -19,3 +19,25 @@ class Solution:
             arr[curr] = -1
             
         return False
+
+
+class SolutionBacktrack:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        
+        def backtrack(curr):
+            
+            if arr[curr] == 0:
+                return True
+            elif arr[curr] == -1:
+                return False
+            
+            temp = arr[curr]
+            for next_index in [curr + arr[curr], curr - arr[curr]]:
+                arr[curr] = -1
+                if 0 <= next_index and next_index < len(arr):
+                    if backtrack(next_index):
+                        return True
+                arr[curr] = temp
+            return False
+                
+        return backtrack(start)
