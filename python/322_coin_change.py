2eae7e6a269054a6b45ff1ff1fd32f144b0a1246
Lisong Guo <lisong.guo@me.com>
2020-07-30 11:28:52 +0200

    add the problem description

--- python/322_coin_change.py
+++ python/322_coin_change.py
@@ -1,3 +1,14 @@
+"""
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+
+    Input: coins = [1, 2, 5], amount = 11
+    Output: 3 
+    Explanation: 11 = 5 + 5 + 1
+
+"""
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         """ with memoization, still Time Limit Exceeded """
