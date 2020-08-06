30a609e171d0fbc8c3fe540168c9a93282790c4c
Lisong Guo <lisong.guo@me.com>
2020-08-06 10:31:24 +0200

    DP with sliding window

--- python/53_Maximum_Subarray.py
+++ python/53_Maximum_Subarray.py
@@ -8,3 +8,16 @@ class Solution:
             dp[i+1] = max(dp[i]+num, num)
         
         return max(dp)
+
+
+class SolutionSlidingWindow:
+    def maxSubArray(self, nums: List[int]) -> int:
+                
+        prev_sum, next_sum, max_sum = float('-inf'), 0, float('-inf')
+        
+        for num in nums:    
+            next_sum = max(prev_sum + num, num)
+            prev_sum = next_sum
+            max_sum = max(max_sum, next_sum)
+            
+        return max_sum
