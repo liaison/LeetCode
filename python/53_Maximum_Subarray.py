64fb949ffe0ba7fbcf0e43c8e1b7492c8bad93b7
Lisong Guo <lisong.guo@me.com>
2020-08-06 10:45:49 +0200

    DP with the exact subarray solution

--- python/53_Maximum_Subarray.py
+++ python/53_Maximum_Subarray.py
@@ -21,3 +21,26 @@ class SolutionSlidingWindow:
             max_sum = max(max_sum, next_sum)
             
         return max_sum
+
+
+class SolutionExactSubarray:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        dp = [(0, 0)] * (len(nums)+1)
+        # (max subarray sum sofar, starting index of subarray)
+        dp[0] = (float('-inf'), -1)
+        
+        for index, num in enumerate(nums):
+            
+            prev_sum, prev_index = dp[index]
+            
+            if num > prev_sum + num:
+                dp[index+1] = (num, index)
+            else:
+                dp[index+1] = (prev_sum + num, prev_index)
+        
+        # for a follow-up question:
+        # return the max sum as well as the exact subarray
+        max_sum, start_index = max(dp, key=lambda x:x[0])
+        
+        return max_sum
