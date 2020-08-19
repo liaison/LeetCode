c04186b45c04bedcc015e81a76d7cf5c4a3750aa
Lisong Guo <lisong.guo@me.com>
2020-08-19 18:30:51 +0200

    backtracking

--- python/78_subsets.py
+++ python/78_subsets.py
@@ -15,3 +15,24 @@ class Solution:
         backtrack(0, [])
         
         return results
+
+
+class SolutionBacktrack:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        results = []
+
+        def backtrack(curr, comb):
+            if curr == len(nums):
+                results.append(list(comb))
+                return results
+            # binary tree branches
+            # take the current number into subset or not
+            comb.append(nums[curr])
+            backtrack(curr+1, comb)
+            comb.pop()
+            backtrack(curr+1, comb)
+        
+        backtrack(0, [])
+        
+        return results
