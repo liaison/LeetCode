d00a44511afec189130be0945c1b44fb9f0f7986
Lisong Guo <lisong.guo@me.com>
2020-08-19 22:43:04 +0200

    string manipulation, str.replace().strip().split()

--- python/1087_brace_expansion.py
+++ python/1087_brace_expansion.py
@@ -1,3 +1,26 @@
+class SolutionStrManipulation:
+    def expand(self, S: str) -> List[str]:
+        
+        blocks = S.replace("{", " ").replace("}", " ").strip().split()
+        dict_list = [sorted(block.split(',')) for block in blocks]
+        
+        results = []
+        
+        def backtrack(curr, comb):
+            if curr == len(dict_list):
+                results.append("".join(comb))
+                return
+            
+            for letter in dict_list[curr]:
+                comb[curr] = letter
+                backtrack(curr+1, comb)
+        
+        comb = [""] * len(dict_list)
+        backtrack(0, comb)
+        
+        return results
+
+
 class Solution:
     def expand(self, S: str) -> List[str]:
         
