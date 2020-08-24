7188a135141031d377bead318a13ee5e26413895
Lisong Guo <lisong.guo@me.com>
2020-08-24 17:47:49 +0200

    greedy scanning with a sliding window

--- python/45_Jump_Game_II.py
+++ python/45_Jump_Game_II.py
@@ -17,3 +17,33 @@ class Solution:
             return min_jumps
         
         return backtrack(0)
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+            Greedy scanning with the sliding window
+        """
+        if len(nums) < 2:
+            # no need to jump
+            return 0
+        
+        next_scope = nums[0]
+        curr_reach = nums[0]
+        
+        # starting from a simple jump
+        jumps = 1
+        
+        for curr, steps in enumerate(nums):
+            
+            # when the index exceeds the reaching window
+            if curr > curr_reach:
+                jumps += 1
+                # we jump to the maximum distance thanks to the sliding window
+                curr_reach = next_scope
+            
+            # sliding window to extend the scope of the reach
+            next_scope = max(next_scope, curr + steps) 
+    
+    
+        return jumps
