e6505130b1e2a3739eec717a4634ed5cc3a59049
Lisong Guo <lisong.guo@me.com>
2020-09-17 22:11:08 +0200

    so-called Fisher-Yates algorithm

---
+++ python/384_Shuffle_Array.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = nums
+        self.perm = list(self.original)
+    
+    
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        # restore the current state of permutation to its original state
+        self.perm = list(self.original)
+        return self.perm
+           
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        # apparently this is so-call Fisher-Yates algorithm.
+        for i in range(len(self.perm)):
+            randi = random.randrange(i, len(self.perm))
+            self.perm[i], self.perm[randi] = self.perm[randi], self.perm[i]
+            
+        return self.perm
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
