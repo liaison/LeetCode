3ade4d3f35fff2024c39de366c7307bd08039a77
Lisong Guo <lisong.guo@me.com>
2020-10-02 22:46:50 +0200

    Shunting Yard algorithm to evaluate an arithematic expression

---
+++ python/227_Basic_Calculator_II.py
@@ -0,0 +1,55 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        """ https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+        """
+        def calculate():
+            operand_2 = operand_stack.pop()
+            operand_1 = operand_stack.pop()
+            operator = operator_stack.pop()
+            
+            if operator == '*':
+                result = operand_1 * operand_2
+            elif operator == "/":
+                result = int(operand_1 / operand_2)
+            elif operator == "+":
+                result = operand_1 + operand_2
+            else:
+                result = operand_1 - operand_2
+        
+            operand_stack.append(result)
+                
+        
+        s = s.replace(" ", '')
+        
+        operand_stack = []
+        operator_stack = []
+        
+        operand = []
+        priority = {"*": 2, "/": 2, "+": 1, "-": 1}
+        
+        for letter in s:
+            if not letter.isnumeric():
+                
+                operand_str = "".join(operand)
+                operand = []
+                operand_stack.append(int(operand_str))
+                
+                if len(operator_stack) == 0:
+                    operator_stack.append(letter)
+                    continue
+                
+                while operator_stack and priority[operator_stack[-1]] >= priority[letter]:
+                    calculate()
+                
+                operator_stack.append(letter)
+            else:
+                operand.append(letter)
+        
+        # add the last operand
+        operand_stack.append(int("".join(operand)))
+        
+        # calculate the rest of the expressions
+        while operator_stack:
+            calculate()
+            
+        return operand_stack[-1]
