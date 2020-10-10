38c46c3809c088dba643f325986a1353c4d2b145
Lisong Guo <lisong.guo@me.com>
2020-10-11 00:01:31 +0200

    find the top 2 largest elements in a list without sorting

--- python/1522_diameter_of_N-ary_tree.py
+++ python/1522_diameter_of_N-ary_tree.py
@@ -20,7 +20,7 @@ class Solution:
             if len(node.children) == 0:
                 return curr_depth
             
-            depths= []
+            depths = []
             for child in node.children:
                 depths.append(max_depth(child, curr_depth+1))
              
@@ -34,3 +34,38 @@ class Solution:
         max_depth(root, 0)
         
         return max_diameter
+
+
+class SolutionNoSorting:
+    def diameter(self, root: 'Node') -> int:
+        """
+        :type root: 'Node'
+        :rtype: int
+        """
+        max_diameter = 0
+        
+        def max_depth(node, curr_depth):
+            nonlocal max_diameter
+            
+            if len(node.children) == 0:
+                return curr_depth
+            
+            max_depth_1, max_depth_2 = 0, curr_depth
+            for child in node.children:
+                depth = max_depth(child, curr_depth+1)
+                if depth > max_depth_1:
+                    max_depth_1, max_depth_2 = depth, max_depth_1
+                elif depth > max_depth_2:
+                    max_depth_2 = depth
+            
+            # calculate the distance between two farthest children
+            max_diameter = max(max_diameter,
+                max_depth_1 + max_depth_2 - 2 * curr_depth)
+            
+            return max_depth_1
+        
+        max_depth(root, 0)
+        
+        return max_diameter
+
+                
