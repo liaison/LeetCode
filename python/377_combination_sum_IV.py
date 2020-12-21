385b97bf0859c962e9e76120ae63a004cfe43cad
Lisong <lisong.guo@me.com>
2020-12-21 20:44:24 +0100

    377. comments on the intuition

--- python/377_combination_sum_IV.py
+++ python/377_combination_sum_IV.py
@@ -1,6 +1,14 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
 
+        """
+            dynamic programming:
+                comb[target] = sum(comb[target-nums[i]])
+
+               - treat each candidate number as the last number in the final combination.
+               - append the candidate number to the combinations of (target - nums[i]), i.e. comb[target-nums[i]]
+        """
+    
         #import functools
         @functools.lru_cache(maxsize = None)
         def combinationSum4(remain):
