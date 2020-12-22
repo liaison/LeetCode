d779a6dd900fc8a960308e046a17bc14f76f2cf0
Lisong <lisong.guo@me.com>
2020-12-22 16:45:09 +0100

    a refined DP bottom-up solution

--- python/322_coin_change.py
+++ python/322_coin_change.py
@@ -4,7 +4,7 @@ You are given coins of different denominations and a total amount of money amoun
 Example 1:
 
     Input: coins = [1, 2, 5], amount = 11
-    Output: 3 
+    Output: 3
     Explanation: 11 = 5 + 5 + 1
 
 """
@@ -15,22 +15,22 @@ class Solution:
 
         @lru_cache(maxsize=None)
         def dfs(sub_amount, count):
-            
+
             if sub_amount == 0:
                 return count
             elif sub_amount < 0:
                 return -1
-            
+
             min_count = float("inf")
             for coin in coins:
-                
+
                 new_count = dfs(sub_amount - coin, count+1)
                 if new_count > 0:
                     min_count = min(min_count, new_count)
-            
+
             return -1 if min_count == float("inf") else min_count
-                
-        
+
+
         return dfs(amount, 0)
 
 
@@ -40,7 +40,7 @@ class SolutionDP:
         dp = [-1] * (amount+1)
         dp[0] = 0
         for sub_amount in range(1, amount+1):
-            
+
             for coin in coins:
                 if sub_amount - coin >= 0:
                     if dp[sub_amount-coin] >= 0:
@@ -49,5 +49,23 @@ class SolutionDP:
                         else:
                             dp[sub_amount] = min(
                                 dp[sub_amount], dp[sub_amount-coin] + 1)
-        
+
         return dp[amount]
+
+
+class SolutionRefinedDP:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        # set the 'inf' so that we could use less conditions in the following loop.
+        dp = [float('inf') for i in range(amount+1)]
+        dp[0] = 0
+
+        for coin in coins:
+            for sub_amount in range(coin, amount+1):
+                dp[sub_amount] = min(dp[sub_amount], dp[sub_amount-coin]+1)
+
+        return -1 if dp[amount] == float('inf') else dp[amount]
+
+
+
+
