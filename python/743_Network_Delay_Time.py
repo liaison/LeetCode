dd760ccf7d4c3c566d9c7d6022f12e70a0a8b632
Lisong <lisong.guo@me.com>
2020-12-29 22:45:23 +0100

    Dijkstra's algorithm. Shortest distance to each of the nodes in a graph

--- python/743_Network_Delay_Time.py
+++ python/743_Network_Delay_Time.py
@@ -1,5 +1,10 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        """
+            The original Dijkstra algorithm is to find the shorting distances
+                between a starting node and each of the rest nodes.
+            This problem can be considered as a variant of Dijkstra problem.
+        """
 
         graph = defaultdict(list)
         for entry in times:
@@ -27,3 +32,34 @@ class Solution:
 
         return clocks if visit_cnt == N else -1
 
+
+class SolutionDijkstra:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        """
+            Dijkstra with heap
+        """
+        graph = defaultdict(list)
+        for entry in times:
+            source, target, weight = entry
+            graph[source].append((weight, target))
+
+        queue = graph[K]
+        heapq.heapify(queue)
+
+        min_elapse = {K:0}
+
+        while queue:
+            # one of the cases is that there might be several edges between two nodes.
+            # the most optimal one would be picked,
+            # therefore we should ignore the rest of the edges.
+            clocks, target = heapq.heappop(queue)
+            if target in min_elapse:
+                continue
+
+            min_elapse[target] = clocks
+            for next_weight, next_target in graph[target]:
+
+                if next_target not in min_elapse:
+                    heapq.heappush(queue, (clocks + next_weight, next_target))
+
+        return max(min_elapse.values()) if len(min_elapse) == N else -1
