3ba65c4b9705d1d34e62a42777a92b82e2b374df
Lisong <lisong.guo@me.com>
2020-12-29 17:26:12 +0100

    Bidirectional BFS. alternating the head and tail layers

--- python/1345_Jump_Game_IV.py
+++ python/1345_Jump_Game_IV.py
@@ -36,3 +36,52 @@ class Solution:
 
         return steps
 
+
+
+class SolutionBidirectionalBFS:
+    def minJumps(self, arr) -> int:
+
+        # value: [indices]
+        graph = defaultdict(set)
+
+        for index, value in enumerate(arr):
+            graph[value].add(index)
+
+
+        visited = [False for i in range(len(arr))]
+        target = len(arr) - 1
+
+        head_queue = set([0])
+        tail_queue = set([target])
+        steps = 0
+
+        # bidirectional BFS
+        # alternating the queue in the forward and backward directions
+        # pick the queue with less elements to advance
+        while head_queue:
+            if len(tail_queue) < len(head_queue):
+                head_queue, tail_queue = tail_queue, head_queue
+
+            next_queue = set()
+            while head_queue:
+                curr = head_queue.pop()
+
+                if curr in tail_queue:
+                    return steps
+                visited[curr] = True
+
+                for neighbor in graph[arr[curr]]:
+                    if not visited[neighbor]:
+                        next_queue.add(neighbor)
+
+                graph[arr[curr]].clear()
+
+                for neighbor in [curr-1, curr+1]:
+                    if 0 <= neighbor <= target and not visited[neighbor]:
+                        next_queue.add(neighbor)
+
+            head_queue = next_queue
+            steps += 1
+
+        return steps
+
