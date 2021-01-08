46caf6bcdf08c065b5b9f56a9d179bec5c27758e
Lisong <lisong.guo@me.com>
2021-01-08 19:35:40 +0100

    backtracking

---
+++ python/306_Additive_Number.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+
+        def backtrack(prev_sum_str, prev_num_str, start, count):
+
+            if start > len(num) - 1:
+                return True if count >= 3 else False
+            elif len(num) - start + 1 < len(prev_sum_str):
+                return False
+            elif num[start:(start+len(prev_sum_str))] != prev_sum_str:
+                return False
+            else:
+                next_start = start + len(prev_sum_str)
+                next_sum = int(prev_sum_str) + int(prev_num_str)
+                return backtrack(str(next_sum), prev_sum_str, next_start, count+1)
+
+
+        for first_sep in range(1, len(num)-1):
+            first_num = int(num[0:first_sep])
+
+            if first_sep > 1 and first_num < 10:
+                break
+
+            for second_sep in range(first_sep+1, len(num)):
+
+                prev_num = int(num[first_sep:second_sep])
+                if second_sep - first_sep > 1 and prev_num < 10:
+                    break
+
+                prev_sum = first_num + prev_num
+                if backtrack(str(prev_sum), str(prev_num), second_sep, 2):
+                    return True
+
+        return False
+
+
