caaca95b89f297b37fe20c36b3a90c80474a9240
Lisong <lisong.guo@me.com>
2021-01-25 17:05:55 +0100

    variant of Dijkstra algorithm to find the minimal distance from one node to the rest of nodes

--- python/1631_path_with_minimal_efforts.py
+++ python/1631_path_with_minimal_efforts.py
@@ -1,37 +1,72 @@
-class Solution:
+class SolutionDijkstra:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        
+
         rows, cols = len(heights), len(heights[0])
-        
+
+        distances = [[float('inf')] * cols for _ in range(rows)]
+        visited = [[False] * cols for _ in range(rows)]
+
+        distances[0][0] = 0
+        queue = [(0, 0, 0)]
+        heapq.heapify(queue)
+
+        while queue:
+            effort, row, col = heapq.heappop(queue)
+
+            visited[row][col] = True
+
+            for (ro, co) in [(0, -1), (-1, 0), (0, 1), (1, 0)]: # left, up, right, down
+                next_row, next_col = row + ro, col + co
+                if 0 <= next_row < rows and 0 <= next_col < cols \
+                    and not visited[next_row][next_col]:
+
+                    new_effort = abs(heights[next_row][next_col] - heights[row][col])
+
+                    new_effort = max(new_effort, distances[row][col])
+
+                    if new_effort < distances[next_row][next_col]:
+                        distances[next_row][next_col] = new_effort
+
+                        heapq.heappush(queue, (new_effort, next_row, next_col))
+
+        return distances[-1][-1]
+
+
+
+class SolutionBinarySearch:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        rows, cols = len(heights), len(heights[0])
+
         def canReach(curr, visited, min_effort):
             if curr == (rows-1, cols-1):
                 return True
-            
-            # right, down, left, up, 
+
+            # right, down, left, up,
             directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
             row, col = curr
             for (ro, co) in directions:
-                next_row, next_col = row + ro, col + co    
+                next_row, next_col = row + ro, col + co
                 if 0 <= next_row < rows and 0 <= next_col < cols:
                     if not visited[next_row][next_col]:
                         effort = abs(heights[next_row][next_col] - heights[row][col])
-            
+
                         if effort > min_effort:
                             continue
 
                         visited[next_row][next_col] = True
                         if canReach((next_row, next_col), visited, min_effort):
                             return True
-                
+
             return False
-        
+
 
         low, high = 0, 10000000
         mid = 0
-        
+
         while low < high:
             mid = (low + high) // 2
-            
+
             curr = (0, 0)
             visited = [[False] * cols for _ in range(rows)]
             visited[0][0] = True
@@ -39,57 +74,56 @@ class Solution:
                 high = mid
             else:
                 low = mid + 1
-            
+
         return low
-    
-    
+
+
 
 
 class Solution:
-    
+
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         """
             Time Limit Exceeded
         """
         rows, cols = len(heights), len(heights[0])
-        
+
         min_effort = float("inf")
-        
+
         def backtrack(curr, visited, prev_effort):
             nonlocal min_effort
-                        
+
             if curr == (rows-1, cols-1):
                 min_effort = min(prev_effort, min_effort)
                 return True
-            
-            # right, down, left, up, 
+
+            # right, down, left, up,
             directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
             row, col = curr
             next_steps = []
             for (ro, co) in directions:
-                next_row, next_col = row + ro, col + co    
+                next_row, next_col = row + ro, col + co
                 if 0 <= next_row < rows and 0 <= next_col < cols:
                     if (next_row, next_col) not in visited:
                         effort = abs(heights[next_row][next_col] - heights[row][col])
                         next_steps.append((next_row, next_col, effort))
-            
+
             #next_steps.sort(key = lambda x: x[2])
-            
+
             for (row, col, effort) in next_steps:
                 visited.add((row, col))
                 next_effort = max(prev_effort, effort)
                 backtrack((row, col), visited, next_effort)
                 visited.remove((row, col))
-            
+
         curr = (0, 0)
         visited = set()
         visited.add((0,0))
-        
-        backtrack(curr, visited, 0)
-        
-        return min_effort
-    
-    
-        
 
-                    
+        backtrack(curr, visited, 0)
+
+        return min_effort
+
+
+
+
