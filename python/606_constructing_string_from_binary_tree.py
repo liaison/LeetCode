4ffe5024bceb469e8e50d865c73f98aad9f28237
Lisong <lisong.guo@me.com>
2021-01-25 21:54:54 +0100

    more concise solution

--- python/606_constructing_string_from_binary_tree.py
+++ python/606_constructing_string_from_binary_tree.py
@@ -36,4 +36,39 @@ class Solution:
         return node_to_str(t)
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class SolutionRefined:
+    def tree2str(self, t: TreeNode) -> str:
+
+        def node_to_str(node):
+
+            if node is None:
+                return ""
+
+            result_list = [str(node.val)]
+
+            left_str = node_to_str(node.left)
+            right_str = node_to_str(node.right)
+
+            if left_str == "":
+                if right_str != "":
+                    result_list.append("()")
+            else:
+                result_list.append("(" + left_str + ")")
+
+            if right_str != "":
+                result_list.append("(" + right_str + ")")
+
+            return "".join(result_list)
+
+
+        return node_to_str(t)
+
+
+
 
