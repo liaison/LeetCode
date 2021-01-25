0bb1b87e322caaefb86c7c6cf3104871d6f1524e
Lisong <lisong.guo@me.com>
2021-01-25 22:43:55 +0100

    using the hashtable to count the occurence

--- python/652_find_duplicated_subtrees.py
+++ python/652_find_duplicated_subtrees.py
@@ -47,6 +47,45 @@ class Solution:
 
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class SolutionCount:
+    def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+
+        # node_str -> count
+        node_str_count = defaultdict(int)
+        duplicated_nodes = list()
+
+        def node2str(node):
+            """
+                this function accomplishes two tasks:
+                    - index each node into a string
+                    - search the duplicated nodes during the traversal
+            """
+            nonlocal node_str_count
+            nonlocal duplicated_nodes
+
+            if node is None:
+                return ""
+
+            node_str = "{}({})({})".format(
+                node.val, node2str(node.left), node2str(node.right))
+
+            node_str_count[node_str] += 1
+            if node_str_count[node_str] == 2:
+                duplicated_nodes.append(node)
+
+            return node_str
+
+
+        node2str(root)
+
+        return duplicated_nodes
+
 
 
 
