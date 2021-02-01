ec95726834334e6a3f001cbbf34790f71f63f6c2
Lisong <lisong.guo@me.com>
2021-02-01 18:34:06 +0100

    rename the class file for Java convention

--- java/JumpGameIV_1345.java
+++ java/JumpGameIV_1345.java
@@ -1,4 +1,11 @@
-class Solution {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+class JumpGameIV_1345 {
+
     public int minJumps(int[] arr) {
 
         HashMap<Integer, List<Integer>> valueMap = new HashMap<>();
