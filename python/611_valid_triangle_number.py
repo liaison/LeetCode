98dfcda6ddfcc30b9a6efb2c5dad1ce81647f04c
Lisong <lisong.guo@me.com>
2021-02-16 21:50:51 +0100

    three-pointers solution, similar with 3SUM

--- python/611_valid_triangle_number.py
+++ python/611_valid_triangle_number.py
@@ -22,3 +22,26 @@ class Solution:
         backtrack(0, [])
 
         return triangle_cnt
+
+
+class SolutionThreePointers:
+    def triangleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        triangle_cnt = 0
+        nums.sort()
+
+        for third in range(n-1, 1, -1):
+            # fixing on the third side
+            first = 0
+            second = third - 1
+            while first < second:
+                if nums[first] + nums[second] > nums[third]:
+                    triangle_cnt += second - first
+                    # try another candidate for the second side
+                    second -= 1
+                else:
+                    # the first side is too small
+                    first += 1
+
+        return triangle_cnt
