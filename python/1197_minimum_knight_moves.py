6b365e151169f466004ee30388463786b7032391
Lisong <lisong.guo@me.com>
2021-04-06 00:24:34 +0200

    bidirectional BFS

--- python/1197_minimum_knight_moves.py
+++ python/1197_minimum_knight_moves.py
@@ -30,3 +30,39 @@ class Solution:
                 steps += 1
 
         return bfs(x, y)
+
+
+
+class BidirectionalBFSSolution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+
+        offsets = [(1, 2), (2, 1), (2, -1), (1, -2),
+                   (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
+
+        origin_queue = deque([(0, 0, 0)])
+        origin_distance = {(0, 0): 0}
+
+        target_queue = deque([(x, y, 0)])
+        target_distance = {(x, y): 0}
+
+        while True:
+
+            origin_x, origin_y, origin_steps = origin_queue.popleft()
+            if (origin_x, origin_y) in target_distance:
+                return origin_steps + target_distance[(origin_x, origin_y)]
+
+            target_x, target_y, target_steps = target_queue.popleft()
+            if (target_x, target_y) in origin_distance:
+                return target_steps + origin_distance[(target_x, target_y)]
+
+            for offset_x, offset_y in offsets:
+                next_origin_x, next_origin_y = origin_x + offset_x, origin_y + offset_y
+
+                if (next_origin_x, next_origin_y) not in origin_distance:
+                    origin_queue.append((next_origin_x, next_origin_y, origin_steps + 1))
+                    origin_distance[(next_origin_x, next_origin_y)] = origin_steps + 1
+
+                next_target_x, next_target_y = target_x + offset_x, target_y + offset_y
+                if (next_target_x, next_target_y) not in target_distance:
+                    target_queue.append((next_target_x, next_target_y, target_steps + 1))
+                    target_distance[(next_target_x, next_target_y)] = target_steps + 1
