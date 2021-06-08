6ccdc66dca27447f8d051383a38745ed28ddc0ae
Lisong <lisong.guo@me.com>
2021-06-08 20:31:10 +0200

    dictionary of all prefixes

--- python/14_longest_common_prefix.py
+++ python/14_longest_common_prefix.py
@@ -16,3 +16,25 @@ class Solution:
             prefix = common_prefix(prefix, word)
 
         return prefix
+
+
+    def longestCommonPrefix_dict(self, strs: List[str]) -> str:
+
+        prefix_dict = defaultdict(int)
+
+        def indexing_prefix(word):
+            nonlocal prefix_dict
+            for index in range(1, len(word)+1):
+                prefix = word[:index]
+                prefix_dict[prefix] += 1
+
+        for word in strs:
+            indexing_prefix(word)
+
+        max_prefix = ""
+        for prefix, count in prefix_dict.items():
+            if count == len(strs):
+                if len(prefix) > len(max_prefix):
+                    max_prefix = prefix
+
+        return max_prefix
