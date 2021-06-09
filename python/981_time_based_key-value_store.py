273bc3756ae94fd737e63870cbbd7720be7cfd52
Lisong <lisong.guo@me.com>
2021-06-09 15:03:05 +0200

    find the right boundary of binary search

--- python/981_time_based_key-value_store.py
+++ python/981_time_based_key-value_store.py
@@ -12,19 +12,23 @@ class TimeMap:
         if key in self.value_dict:
             value_list = self.value_dict[key]
 
-        to_insert = self.binary_search(value_list, timestamp)
+        to_insert = self.binary_search_right(value_list, timestamp)
         value_list.insert(to_insert, (value, timestamp))
         self.value_dict[key] = value_list
 
 
-    def binary_search(self, values, timestamp):
+    def binary_search_right(self, values, timestamp):
+        """
+            return the index so that values[index:] >= timestamp
+        """
         low, high = 0, len(values)
         while low < high:
             mid = (low + high) // 2
-            if values[mid][1] < timestamp:
-                low = mid + 1
-            else:
+            if timestamp < values[mid][1]:
                 high = mid
+            else:
+                low = mid + 1
+
         return low
 
 
@@ -33,8 +37,7 @@ class TimeMap:
             return ""
 
         values = self.value_dict[key]
-
-        low = self.binary_search(values, timestamp)
+        low = self.binary_search_right(values, timestamp)
 
         # beyond the current list
         if low >= len(values):
@@ -54,4 +57,11 @@ class TimeMap:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+
+
+
+
+
+
 
