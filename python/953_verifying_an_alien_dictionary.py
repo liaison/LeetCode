669519a15733654ccb5b3f443a55335f40dac0d1
Lisong <lisong.guo@me.com>
2021-06-09 12:18:02 +0200

    sliding window with a customized comparator

---
+++ python/953_verifying_an_alien_dictionary.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+
+        letter_order = {}
+        for rank, letter in enumerate(order):
+            letter_order[letter] = rank
+
+        def is_less_than(word1, word2):
+            bound = min(len(word1), len(word2))
+            index = 0
+            while index < bound:
+                if letter_order[word1[index]] < letter_order[word2[index]]:
+                    return True
+                elif letter_order[word1[index]] > letter_order[word2[index]]:
+                    return False
+                index += 1
+
+            return True if len(word1) <= len(word2) else False
+
+
+        prev_word = words[0]
+        for next_word in words[1:]:
+            if not is_less_than(prev_word, next_word):
+                return False
+            prev_word = next_word
+
+
+        return True
+
