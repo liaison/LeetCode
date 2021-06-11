b0d60f356211bbb626793e5b914f231d3f7eeee3
Lisong <lisong.guo@me.com>
2021-06-11 23:33:36 +0200

    one hashmap

--- python/383_ransom_note.py
+++ python/383_ransom_note.py
@@ -20,3 +20,22 @@ class Solution:
 
         return True
 
+
+    def canConstruct_OneHashmap(self, ransomNote: str, magazine: str) -> bool:
+
+        def count_letter(word):
+            letter_cnt = defaultdict(int)
+            for letter in word:
+                letter_cnt[letter] += 1
+            return letter_cnt
+
+        magazine_dict = count_letter(magazine)
+
+        for letter in ransomNote:
+            if letter not in magazine_dict:
+                return False
+            magazine_dict[letter] -= 1
+            if magazine_dict[letter] == 0:
+                del magazine_dict[letter]
+
+        return True
