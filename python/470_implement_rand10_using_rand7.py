739559e8fec7bbe57a56dc4e631fcb71263a8e6f
Lisong <lisong.guo@me.com>
2021-06-11 19:34:56 +0200

    rejection sampling

--- python/470_implement_rand10_using_rand7.py
+++ python/470_implement_rand10_using_rand7.py
@@ -6,14 +6,14 @@ class Solution:
     def rand10(self):
         """
         :rtype: int
-        
+
             Two-phase generation, decision-tree alike approach
-                          
+
                  [1, 2, 3]            [5, 6, 7]
                      |                    |
                      |                    |
               [1, 2, 3, 4, 5]      [6, 7, 8, 9, 10]
-              
+
         """
         while True:
             first_round = rand7()
@@ -22,3 +22,17 @@ class Solution:
                     second_round = rand7()
                     if second_round <= 5:
                         return second_round + (first_round < 4) * 5
+
+
+    def rand10_rejection_sampling(self):
+        """
+        :rtype: int
+        """
+        while True:
+            row, col = rand7(), rand7()
+            index = (row - 1) * 7 + col
+            if index <= 40:
+                return (index - 1) % 10 + 1
+
+
+
