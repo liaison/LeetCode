694f7491a869dc869e6c2bb4022907015b6d2b82
Lisong <lisong.guo@me.com>
2021-06-13 18:24:24 +0200

    minor optimization

--- python/676_implement_magic_dictionary.py
+++ python/676_implement_magic_dictionary.py
@@ -59,3 +59,67 @@ class MagicDictionary:
 # obj = MagicDictionary()
 # obj.buildDict(dictionary)
 # param_2 = obj.search(searchWord)
+
+
+class MagicDictionaryMinorOptimized:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie_dict = {}
+
+
+    def buildDict(self, dictionary: List[str]) -> None:
+
+        def index_word(word):
+            word_group = len(word)
+            if word_group not in self.trie_dict:
+                self.trie_dict[word_group] = {}
+
+            curr_node = self.trie_dict[word_group]
+
+            for letter in word:
+                if letter not in curr_node:
+                    curr_node[letter] = {}
+                curr_node = curr_node[letter]
+
+        for word in dictionary:
+            index_word(word)
+
+
+    def search(self, searchWord: str) -> bool:
+
+        def dfs(node, index, unmatched_once):
+            if index == len(searchWord):
+                return unmatched_once
+
+            letter = searchWord[index]
+
+            # minor optimization to reduce the total number of iterations
+            # When we have one unmatched before, the next letter must be matched.
+            if unmatched_once:
+                if letter not in node:
+                    return False
+                else:
+                    return dfs(node[letter], index+1, True)
+
+            for next_letter in node.keys():
+                unmatched = (letter != next_letter)
+                if dfs(node[next_letter], index+1, unmatched):
+                    return True
+
+            return False
+
+        word_group = len(searchWord)
+        if word_group not in self.trie_dict:
+            return False
+
+        start_node = self.trie_dict[word_group]
+        return dfs(start_node, 0, False)
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
