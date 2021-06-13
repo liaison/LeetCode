208fa366100a378d9d205b4e7a60ec5a0695c02f
Lisong <lisong.guo@me.com>
2021-06-13 19:17:36 +0200

    UnionFind solution

--- python/261_graph_valid_tree.py
+++ python/261_graph_valid_tree.py
@@ -31,3 +31,44 @@ class Solution:
 
 
 
+class UnionFind:
+    def __init__(self, size):
+        self.group = [i for i in range(size)]
+
+    def find(self, node):
+        if self.group[node] != node:
+            self.group[node] = self.find(self.group[node])
+        return self.group[node]
+
+    def union(self, n1, n2):
+        """
+            return false: if n1 and n2 already belong to the same group
+        """
+        g1 = self.find(n1)
+        g2 = self.find(n2)
+        if g1 != g2:
+            self.group[g1] = self.group[g2]
+            return True
+        else:
+            return False
+
+
+class SolutionUnionFind:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        if len(edges) != n-1:
+            return False
+
+        uf = UnionFind(n)
+
+        for edge in edges:
+            start, end = edge[0], edge[1]
+            if not uf.union(start, end):
+                return False
+
+        return True
+
+
+
+
+
