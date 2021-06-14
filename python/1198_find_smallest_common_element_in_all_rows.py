17cc13f3752d1fb8e5ad4c94c768d54263535b1b
Lisong <lisong.guo@me.com>
2021-06-14 22:48:32 +0200

    Merge K list

--- python/1198_find_smallest_common_element_in_all_rows.py
+++ python/1198_find_smallest_common_element_in_all_rows.py
@@ -26,3 +26,34 @@ class Solution:
                 return low
 
         return -1
+
+
+
+class SolutionMergeK:
+    def smallestCommonElement(self, mat: List[List[int]]) -> int:
+
+        k_cursors = [0 for _ in range(len(mat))]
+        cursor_num = len(k_cursors)
+        row_length = len(mat[0])
+        curr_max, curr_cnt = 0, 0
+
+        while True:
+            for k in range(cursor_num):
+
+                cursor = k_cursors[k]
+                while cursor < row_length and mat[k][cursor] < curr_max:
+                    cursor += 1
+
+                # exceed one of the rows
+                if cursor == row_length:
+                    return -1
+
+                k_cursors[k] = cursor
+                if mat[k][cursor] > curr_max:
+                    # reset the max value and its counter
+                    curr_max = mat[k][cursor]
+                    curr_cnt = 1
+                else: # mat[k][cursor] == curr_max
+                    curr_cnt += 1
+                    if curr_cnt == cursor_num:
+                        return curr_max
