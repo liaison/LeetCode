6456cdfb643483b8a3c74a5306cfffcbae22ca2a
Lisong <lisong.guo@me.com>
2021-06-14 21:48:38 +0200

    matrix multiplication

--- python/311_Sparse_Matrix_Multiplication.py
+++ python/311_Sparse_Matrix_Multiplication.py
@@ -1,16 +1,16 @@
 class Solution:
-    def multiply(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-        
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+
         # convert the second matrix to column-based vectors
-        col_vecs = [[] for i in range(len(B[0]))]
-        for row in B:
+        col_vecs = [[] for i in range(len(mat2[0]))]
+        for row in mat2:
             for col_index, col in enumerate(row):
                 col_vecs[col_index].append(col)
-        
-        result = [[] for i in range(len(A))]
-        for row_index, row_vec in enumerate(A):
+
+        result = [[] for i in range(len(mat1))]
+        for row_index, row_vec in enumerate(mat1):
             for col_index, col_vec in enumerate(col_vecs):
-                dotsum = sum([a*b for (a, b) in zip(row_vec, col_vec)])
-                result[row_index].append(dotsum)
-        
+                dot_product = sum([a*b for (a, b) in zip(row_vec, col_vec)])
+                result[row_index].append(dot_product)
+
         return result
