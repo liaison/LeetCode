39235824dc284b1bb75bd6ebebfaca57570385f0
Lisong <lisong.guo@me.com>
2021-08-10 20:05:47 +0200

    better way to calculate median number

--- python/1093_statistics_from_a_large_sample.py
+++ python/1093_statistics_from_a_large_sample.py
@@ -57,3 +57,63 @@ class Solution:
         return output
 
 
+
+class Solution:
+    def sampleStats(self, count: List[int]) -> List[float]:
+
+        num_count = []
+        for num, count in enumerate(count):
+            if count != 0:
+                num_count.append((num, count))
+
+        output = []
+        # minimum
+        output.append(num_count[0][0])
+        # maximum
+        output.append(num_count[-1][0])
+
+        total_sum = 0
+        total_count = 0
+        max_count = 0
+        mode = 0
+        for (num, count) in num_count:
+            total_sum += num * count
+            total_count += count
+            if count > max_count:
+                mode = num
+                max_count = count
+
+        # mean
+        output.append(total_sum / total_count)
+
+        # The indices of the median numbers, regardless the total number (even or not)
+        m1 = (total_count + 1) // 2
+        m2 = total_count // 2 + 1
+        median = 0
+        prefix_sum = 0
+        print("m1 m2", m1, m2)
+        for num, count in num_count:
+            if prefix_sum < m1 and prefix_sum + count >= m1:
+                # fetch the first median number
+                median += num
+
+            if prefix_sum < m2 and prefix_sum + count >= m2:
+                # fetch the second median number
+                median += num
+                median = median / 2
+                break
+
+            prefix_sum += count
+
+        # median
+        output.append(median)
+
+        # mode
+        output.append(mode)
+
+        return output
+
+
+
+
+
