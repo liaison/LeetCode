a0e7143bdad5aa6686d56b0ffaa8910ff1db4f81
Lisong <lisong.guo@me.com>
2021-08-11 22:12:50 +0200

    A* algorithm

--- python/1293_shortest_path_in_grid_with_obstacles_elimination.py
+++ python/1293_shortest_path_in_grid_with_obstacles_elimination.py
@@ -33,3 +33,48 @@ class Solution:
 
         # did not reach the target
         return -1
+
+
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+
+        rows, cols = len(grid), len(grid[0])
+        target = (rows-1, cols-1)
+
+        manhattan_distance = lambda row, col: (rows - row + cols - col - 2)
+
+        if manhattan_distance(0, 0) <= k:
+            return rows - 1 + cols - 1
+
+        # (row, col, remaining_elimination)
+        state = (0, 0, k)
+
+        # (estimation, steps, state)
+        queue = [(manhattan_distance(0, 0), 0, state)]
+        seen = set(state)
+
+        while queue:
+            estimation, steps, (row, col, remain_eliminations) = heapq.heappop(queue)
+
+            # we can reach the target in the shortest path (manhattan distance),
+            #   even if the remaining steps are all obstacles
+            if estimation - steps <= remain_eliminations:
+                return estimation
+
+            # explore the four directions in the next step
+            for new_row, new_col in [(row, col+1), (row+1, col), (row, col-1), (row-1, col)]:
+                # out of the boundary of grid
+                if 0 > new_row or new_row == rows or 0 > new_col or new_col == cols:
+                    continue
+
+                new_eliminations = remain_eliminations - grid[new_row][new_col]
+                new_state = (new_row, new_col, new_eliminations)
+
+                # if the next direction is worth exploring
+                if new_eliminations >= 0 and new_state not in seen:
+                    seen.add(new_state)
+                    new_estimation = manhattan_distance(new_row, new_col) + steps + 1
+                    queue.append((new_estimation, steps+1, new_state))
+
+        # did not reach the target
+        return -1
