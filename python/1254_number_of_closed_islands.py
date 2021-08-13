0674fb815094561eca68409685f0ac34874cff62
Lisong <lisong.guo@me.com>
2021-08-13 10:18:26 +0200

    DFS with stack

--- python/1254_number_of_closed_islands.py
+++ python/1254_number_of_closed_islands.py
@@ -31,3 +31,43 @@ class Solution:
 
 
         return closed_islands
+
+
+class SolutionDFSwithStack:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+
+        def dfs(row, col):
+
+            is_closed = True
+            queue = [(row, col)]
+
+            while queue:
+                # queue that behaves as stack
+                row, col = queue.pop()
+
+                # mark the visited cell
+                grid[row][col] = 1
+
+                if row in [0, len(grid)-1] or col in [0, len(grid[0])-1]:
+                    is_closed = False
+
+                for row, col in [(row, col+1), (row+1, col), (row, col-1), (row-1, col)]:
+                    if 0 > row or row == len(grid) or col < 0 or col == len(grid[0]):
+                        continue
+
+                    if grid[row][col] == 0:
+                        queue.append((row, col))
+
+            return is_closed
+
+        closed_islands = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 0:
+                    # kick off the DFS visit
+                    is_closed = dfs(row, col)
+                    if is_closed:
+                        closed_islands += 1
+
+
+        return closed_islands
