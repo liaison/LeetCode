93b29963f1abfd18a2264aab24b7ea22be7e5d73
Lisong <lisong.guo@me.com>
2021-08-13 12:34:12 +0200

    UnionFind

--- python/1254_number_of_closed_islands.py
+++ python/1254_number_of_closed_islands.py
@@ -71,3 +71,63 @@ class SolutionDFSwithStack:
 
 
         return closed_islands
+
+
+class UnionFind:
+    def __init__(self, size):
+        self.group = [i for i in range(size)]
+
+    def find(self, index):
+        if self.group[index] != index:
+            self.group[index] = self.find(self.group[index])
+        return self.group[index]
+
+    def union(self, a, b):
+        group_a = self.find(a)
+        group_b = self.find(b)
+        if group_a != group_b:
+            # attach the group of the group_b to the group of group_a
+            self.group[group_b] = group_a
+
+
+
+class SolutionUnionFind:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        index = lambda row, col: (row * COLS + col)
+
+        uf = UnionFind(ROWS * COLS)
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 0:
+                    # only needs to check the neighbors on the right and down sides
+                    for next_row, next_col in [(row, col+1), (row+1, col)]:
+                        if next_row == ROWS or next_col == COLS:
+                            continue
+                        if grid[next_row][next_col] == 0:
+                            uf.union(index(row, col), index(next_row, next_col))
+
+
+        # indicate if the island group is closed or not
+        island_mark = dict()
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 0:
+                    group_id = uf.find(index(row, col))
+
+                    if group_id not in island_mark:
+                        island_mark[group_id] = True
+
+                    if row in [0, ROWS-1] or col in [0, COLS-1]:
+                        # one island on the border
+                        island_mark[group_id] = False
+
+        return len([is_closed for is_closed in island_mark.values() if is_closed])
+
+
+
+
+
+
