976fc934dfaa28cd8d387655acd7b70553700149
Lisong <lisong.guo@me.com>
2021-08-16 23:19:42 +0200

    combination sum in DFS with memoization

---
+++ python/1751_maximum_number_of_events_that_can_be_attended_II.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+
+        events.sort(key = lambda x: x[0])
+
+        @functools.lru_cache(maxsize=None)
+        def dfs(event_index, remain_k):
+            if remain_k == 0 or event_index == len(events):
+                return 0
+
+            if remain_k == 1:
+                return max([event[2] for event in events[event_index:]])
+
+            not_included = dfs(event_index+1, remain_k)
+
+            # linear search
+            current_end = events[event_index][1]
+            next_index = len(events)
+            for index in range(event_index+1, len(events)):
+                if events[index][0] > current_end:
+                    next_index = index
+                    break
+
+            included = events[event_index][2] + dfs(next_index, remain_k - 1)
+
+            return max(not_included, included)
+
+
+        return dfs(0, k)
+
