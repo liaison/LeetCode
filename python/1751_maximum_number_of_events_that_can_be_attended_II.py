a2ad78531846c62ae86d71586762f96cb0bc46b4
Lisong <lisong.guo@me.com>
2021-08-16 23:24:53 +0200

    slight optimization with binary search

--- python/1751_maximum_number_of_events_that_can_be_attended_II.py
+++ python/1751_maximum_number_of_events_that_can_be_attended_II.py
@@ -28,3 +28,31 @@ class Solution:
 
         return dfs(0, k)
 
+
+class SolutionWithBinarySearch:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+
+        events.sort(key = lambda x: x[0])
+
+        start_days = [event[0] for event in events]
+
+        @functools.lru_cache(maxsize=None)
+        def dfs(event_index, remain_k):
+            if remain_k == 0 or event_index == len(events):
+                return 0
+
+            if remain_k == 1:
+                return max([event[2] for event in events[event_index:]])
+
+            not_included = dfs(event_index+1, remain_k)
+
+            # binary search on the next available event
+            current_end = events[event_index][1]
+            next_index = bisect.bisect_right(start_days, current_end, lo = event_index+1)
+
+            included = events[event_index][2] + dfs(next_index, remain_k - 1)
+
+            return max(not_included, included)
+
+
+        return dfs(0, k)
