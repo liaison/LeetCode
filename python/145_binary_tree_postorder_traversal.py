5e2122a513cd655afeb8fcb81a38e9e1e25d1c3e
Lisong <lisong.guo@me.com>
2021-08-17 15:36:33 +0200

    DFS with stack

--- python/145_binary_tree_postorder_traversal.py
+++ python/145_binary_tree_postorder_traversal.py
@@ -19,3 +19,27 @@ class Solution:
 
         dfs(root)
         return output
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class SolutionStack:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        output = deque([])
+
+        stack = [root]
+        while stack:
+            node = stack.pop()
+
+            if node:
+                stack.append(node.left)
+                stack.append(node.right)
+
+                output.appendleft(node.val)
+
+        return output
