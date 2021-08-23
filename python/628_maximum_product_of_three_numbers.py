4d997efb458f2e2c5f4e155ed4d0d311ecbab1d6
Lisong <lisong.guo@me.com>
2021-08-23 19:02:23 +0200

    comment

--- python/628_maximum_product_of_three_numbers.py
+++ python/628_maximum_product_of_three_numbers.py
@@ -4,5 +4,8 @@ class Solution:
 
         nums.sort()
 
+        # the maximum value for the product of three numbers can only be the following two cases:
+        #  -  top 3 numbers
+        #  -  top 1 number * bottom 2 numbers
         return max(nums[-1] * nums[-2] * nums[-3],
                    nums[0] * nums[1] * nums[-1])
