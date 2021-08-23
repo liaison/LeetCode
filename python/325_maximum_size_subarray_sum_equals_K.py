1c1fdbbf5b5425f88bd6e2cf21075ba2aa950b36
Lisong <lisong.guo@me.com>
2021-08-23 14:58:40 +0200

    revision

--- python/325_maximum_size_subarray_sum_equals_K.py
+++ python/325_maximum_size_subarray_sum_equals_K.py
@@ -1,22 +1,26 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
-        
+
+        max_subarray = 0
         prefix_sum_index = {}
         prefix_sum = 0
-        max_size = 0
-        
-        for index, num in enumerate(nums):
-            
-            prefix_sum += num
-            
+
+        for index in range(0, len(nums)):
+
+            prefix_sum += nums[index]
+
+            if prefix_sum == k:
+                # use only the prefix
+                max_subarray = max(max_subarray, index + 1)
+
             if prefix_sum not in prefix_sum_index:
                 prefix_sum_index[prefix_sum] = index
-            
-            if prefix_sum == k:
-                max_size = max(max_size, index+1)
-            
-            prev_sum = prefix_sum - k
-            if prev_sum in prefix_sum_index:
-                max_size = max(max_size, index - prefix_sum_index[prev_sum])
-        
-        return max_size
+
+            target = prefix_sum - k
+            if target in prefix_sum_index:
+                prev_index = prefix_sum_index[target]
+                max_subarray = max(max_subarray, index - prev_index)
+
+        return max_subarray
+
+
