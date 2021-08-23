27fb80d4463100414fa6bd06eaea8cc045131ece
Lisong <lisong.guo@me.com>
2021-08-23 11:09:41 +0200

    sliding window with right pointer fixed at each iteration

--- python/1658_Maximum_Operations_to_Reduce_X_to_Zero.py
+++ python/1658_Maximum_Operations_to_Reduce_X_to_Zero.py
@@ -27,3 +27,33 @@ class SolutionTLE:
 
 
         return -1
+
+
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+
+        total_sum = sum(nums)
+
+        # need all elements
+        if total_sum == x:
+            return len(nums)
+
+        # otherwise, only need a subarray
+        target = total_sum - x
+
+        # find the maximum subarray for the remaining sum
+        max_subarray_size = float('-inf')
+        left = 0
+        window_sum = 0
+        for right in range(0, len(nums)):
+            window_sum += nums[right]
+
+            while window_sum > target and left < right:
+                window_sum -= nums[left]
+                left += 1
+
+            if window_sum == target:
+                max_subarray_size = max(max_subarray_size, right - left + 1)
+
+
+        return len(nums) - max_subarray_size if max_subarray_size != float('-inf') else -1
