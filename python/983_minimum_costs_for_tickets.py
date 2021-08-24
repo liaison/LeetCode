b001d560784a009982207dea40d4809d98d938c7
Lisong <lisong.guo@me.com>
2021-08-24 11:37:39 +0200

    DP approach in bottom-up

---
+++ python/983_minimum_costs_for_tickets.py
@@ -0,0 +1,36 @@
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+
+        """
+            Dynamic programming in the bottom-up approach
+        """
+        N = len(days)
+
+        # dp[i] the minimum cost if the travel is of the postfix: days[i:]
+        dp = [0] * (N+1)
+
+        # should be the 1-day pass, if the input is designed properly
+        # i.e. one day should be the least expensive of all.
+        # the minimal cost of ticket if the travel plan consists of one day
+        dp[N-1] = min(costs)
+
+        for index in range(N-2, -1, -1):
+            curr_day = days[index]
+
+            min_cost = float('inf')
+
+            # Try different plans from the current day till the end
+            for ci, duration in enumerate([1, 7, 30]):
+                next_day_index = index
+                while next_day_index < N and days[next_day_index] < curr_day + duration :
+                    next_day_index += 1
+
+                if next_day_index != N:
+                    min_cost = min(min_cost, costs[ci] + dp[next_day_index])
+                else:
+                    min_cost = min(min_cost, costs[ci])
+
+            dp[index] = min_cost
+
+        return dp[0]
