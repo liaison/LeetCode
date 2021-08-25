f715e0785d612b0afc68e65b54496f7e10839528
Lisong <lisong.guo@me.com>
2021-08-25 17:04:33 +0200

    DFS with memoization

---
+++ python/1626_best_team_with_no_conflicts.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+
+        """
+            TLE at the 132/148 test cases
+        """
+        age_score = [(age, score) for age, score in zip(ages, scores)]
+        age_score.sort(key = lambda x: x)
+        age_score.append((0, 0))  # an artificial point as boundary
+
+        @functools.lru_cache(maxsize=None)
+        def dfs(curr_index, last_index):
+
+            if curr_index == len(scores):
+                return 0
+
+            last_age, last_score = age_score[last_index]
+            curr_age, curr_score = age_score[curr_index]
+
+            max_score = 0
+            if curr_age == last_age or curr_score >= last_score:
+                # can add this person to the current team
+                option1 = curr_score + dfs(curr_index+1, curr_index)
+                max_score = max(max_score, option1)
+
+            # skip this person
+            option2 = dfs(curr_index+1, last_index)
+            max_score = max(max_score, option2)
+
+            #print(curr_index, last_index, max_score)
+            return max_score
+
+        return dfs(0, len(scores))
