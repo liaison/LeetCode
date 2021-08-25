dc198e5c90316977177f1090e68ec1892de667d2
Lisong <lisong.guo@me.com>
2021-08-25 17:44:53 +0200

    one-dimentional DP with array

--- python/1626_best_team_with_no_conflicts.py
+++ python/1626_best_team_with_no_conflicts.py
@@ -67,3 +67,34 @@ class Solution:
                 dp[(curr_index, last_index)] = max_score
 
         return dp[(0, N)]
+
+
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+
+        age_score = [(age, score) for age, score in zip(ages, scores)]
+        age_score.sort(key = lambda x: x)
+
+        N = len(scores)
+        # dp[i]:  maximum score for a team
+        #         including "i" with all other possible members before ith element
+        dp = [0] * N
+
+        global_max = 0
+
+        for curr_index in range(0, N):
+
+            curr_age, curr_score = age_score[curr_index]
+            max_score = curr_score
+
+            for prev_index in range(0, curr_index):
+                prev_age, prev_score = age_score[prev_index]
+
+                if prev_score <= curr_score:
+                    max_score = max(max_score, curr_score + dp[prev_index])
+
+            dp[curr_index] = max_score
+            global_max = max(global_max, max_score)
+
+
+        return global_max
