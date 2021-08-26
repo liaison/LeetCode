3deda62a13ee2bacab5c2ed04a59f01e7a53a7f5
Lisong <lisong.guo@me.com>
2021-08-26 19:43:49 +0200

    sliding window with two extreme values at each step

--- python/152_Maximum_Product_Subarray.py
+++ python/152_Maximum_Product_Subarray.py
@@ -6,7 +6,7 @@ class Solution:
         """
         curr_products = []
         max_product = float('-inf')
-        
+
         for num in nums:
             curr_products.append(1)
             next_products = []
@@ -14,28 +14,29 @@ class Solution:
                 new_product = num * product
                 max_product = max(max_product, new_product)
                 next_products.append(new_product)
-            
+
             curr_products = next_products
-            
+
         return max_product
 
 
 class SolutionDP:
     """
-        Rather than keeping all the intermediate products, 
+        Rather than keeping all the intermediate products,
           we take the two extreme numbers among the intermediate results,
-          since only these two extreme numbers can change the final result. 
+          since only these two extreme numbers can change the final result.
     """
     def maxProduct(self, nums: List[int]) -> int:
-        
+
         max_product = nums[0]
         max_sofar, min_sofar = nums[0], nums[0]
-        
+
         for num in nums[1:]:
             new_min_sofar = num * min_sofar
             new_max_sofar = num * max_sofar
-            min_sofar, max_sofar = min(num, new_min_sofar, new_max_sofar), max(num, new_min_sofar, new_max_sofar)
-            
+            min_sofar = min(num, new_min_sofar, new_max_sofar)
+            max_sofar = max(num, new_min_sofar, new_max_sofar)
+
             max_product = max(max_product, max_sofar)
-            
+
         return max_product
