7e563063f87847291f3db03fa3274ec67afdac73
Lisong <lisong.guo@me.com>
2021-08-26 18:37:14 +0200

    DFS with memoization

--- python/265_paint_houses_II.py
+++ python/265_paint_houses_II.py
@@ -27,3 +27,30 @@ class Solution:
 
         # minimal cost to paint all houses (0.1....house_index)
         return min(dp[-1])
+
+
+class SolutionDFS:
+    def minCostII(self, costs: List[List[int]]) -> int:
+
+        n_houses = len(costs)
+        n_colors = len(costs[0])
+
+        @functools.lru_cache(maxsize=None)
+        def dfs(house_index, color_index):
+            if house_index == 0:
+                return 0
+
+            min_cost = float('inf')
+            for next_color in range(n_colors):
+                if next_color == color_index:
+                    continue
+                new_cost = costs[house_index-1][color_index] + dfs(house_index-1, next_color)
+                min_cost = min(min_cost, new_cost)
+
+            return min_cost
+
+        min_cost = float('inf')
+        for color_index in range(n_colors):
+            min_cost = min(min_cost, dfs(n_houses, color_index))
+
+        return min_cost
