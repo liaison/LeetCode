9e4aba7dacc4906fb05a82c90f02346a3bc30ddc
Lisong <lisong.guo@me.com>
2021-08-26 12:06:17 +0200

    Greedy, hashtable, and binary search

---
+++ python/792_number_of_matching_subsequence.py
@@ -0,0 +1,37 @@
+
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+
+        letter_indices = defaultdict(list)
+        for index, letter in enumerate(s):
+            letter_indices[letter].append(index)
+
+        def isSubsequence(word):
+            """
+                Greedy matching which ensures to find a solution if there exists one.
+
+                XYZ    -->    ****X****Y*****Z***
+            """
+            cursor = -1
+            for letter in word:
+                if letter not in letter_indices:
+                    return False
+
+                indices = letter_indices[letter]
+                first_match = bisect.bisect_right(indices, cursor)
+
+                if first_match == len(indices):
+                    # no match
+                    return False
+                else:
+                    cursor = indices[first_match]
+
+            return True
+
+        count = 0
+        for word in words:
+            if isSubsequence(word):
+                count += 1
+        return count
+
