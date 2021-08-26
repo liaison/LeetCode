f1724e854747401a45a44718bb894f5576085ed9
Lisong <lisong.guo@me.com>
2021-08-26 18:16:45 +0200

    comments

--- python/265_paint_houses_II.py
+++ python/265_paint_houses_II.py
@@ -15,18 +15,15 @@ class Solution:
         for house_index in range(1, n_houses+1):
             for curr_color in range(n_colors):
                 min_cost = float('inf')
-
                 for prev_color in range(n_colors):
                     # exclude the current color for the previous house
                     if curr_color == prev_color:
                         continue
-
                     new_cost = costs[house_index-1][curr_color] + dp[house_index-1][prev_color]
-
                     min_cost = min(min_cost, new_cost)
 
-
+                # the minimal cost to paint the current house in 'curr_color'
                 dp[house_index][curr_color] = min_cost
 
-
+        # minimal cost to paint all houses (0.1....house_index)
         return min(dp[-1])
