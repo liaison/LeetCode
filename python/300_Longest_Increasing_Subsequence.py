e8b3e75d5cd19b1d24f3162f325277cec9c538af
Lisong <lisong.guo@me.com>
2021-08-27 17:16:35 +0200

    DP with sliding window

--- python/300_Longest_Increasing_Subsequence.py
+++ python/300_Longest_Increasing_Subsequence.py
@@ -4,10 +4,10 @@ Given an unsorted array of integers, find the length of longest increasing subse
 Example:
 
 Input: [10,9,2,5,3,7,101,18]
-Output: 4 
+Output: 4
 
 
-Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
 
 
 Note:
@@ -25,19 +25,19 @@ class Solution(object):
         """
         if len(nums) == 0:
             return 0
-        
+
         # the longest increasing subsequence for the prefix [0...i]
         dp = [1] * (len(nums))
-        
+
         globalMaxLen = -1
-        
+
         for i, num in enumerate(nums):
             localMaxLen = 1
             for k in range(i):
                 if nums[i] > nums[k]:
                     localMaxLen = max(dp[k]+1, localMaxLen)
-            
+
             dp[i] = localMaxLen
             globalMaxLen = max(localMaxLen, globalMaxLen)
-            
+
         return globalMaxLen
