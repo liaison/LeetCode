8cadeeded2fa3fab41513d3315ad0f882cf09ba6
Lisong <lisong.guo@me.com>
2021-09-02 18:02:43 +0200

    compare the rest of the substrings all the way, rather on the level of individual letter

--- python/1754_largest_merge_of_two_strings.py
+++ python/1754_largest_merge_of_two_strings.py
@@ -7,33 +7,20 @@ class Solution:
         merge = []
         while index_1 < len_1 or index_2 < len_2:
 
-            # one of the pointers reaches the end
             if index_1 == len_1:
                 merge.append(word2[index_2])
                 index_2 += 1
             elif index_2 == len_2:
                 merge.append(word1[index_1])
                 index_1 += 1
-             # both pointers are still within the boundary
-            else:
-                # pick the larger letter
-                if word1[index_1] > word2[index_2]:
+            else: # both index are valid
+                # look ahead of the rest of the substring
+                # pick the larger substring
+                if word1[index_1:] >= word2[index_2:]:
                     merge.append(word1[index_1])
                     index_1 += 1
-                elif word1[index_1] < word2[index_2]:
+                else:
                     merge.append(word2[index_2])
                     index_2 += 1
-                else:
-                    # look ahead of the rest of the substring
-                    if word1[index_1:] >= word2[index_2:]:
-                        merge.append(word1[index_1])
-                        index_1 += 1
-                    else:
-                        merge.append(word2[index_2])
-                        index_2 += 1
 
         return "".join(merge)
-
-
-
-
