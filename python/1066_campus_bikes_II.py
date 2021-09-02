ab0b51294dc834e686a88829917a872794b83398
Lisong <lisong.guo@me.com>
2021-09-02 22:19:38 +0200

    backtracking with memoization

--- python/1066_campus_bikes_II.py
+++ python/1066_campus_bikes_II.py
@@ -37,3 +37,41 @@ class Solution:
 
         backtrack(0)
         return min_distance_sum
+
+
+class Solution:
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
+        """
+            Backtracking with memoization
+        """
+        def manhattan_distance(worker, bike):
+            return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1])
+
+        @functools.lru_cache(maxsize=None)
+        def min_distance_sum(worker_index, bike_mask):
+            """
+                All input parameters need to be "hashtable",
+                  in order to apply the memoization
+            """
+            if worker_index == len(workers):
+                return 0
+
+            # convert the bitmask to list for backtracking later
+            #  since the string is immutable in Python
+            bit_mask = [bit for bit in bike_mask]
+
+            ret = float("inf")
+            for bike_index, bit in enumerate(bike_mask):
+                if bit == "0":
+                    new_match = manhattan_distance(workers[worker_index], bikes[bike_index])
+                    # mark the bike as occupied
+                    bit_mask[bike_index] = "1"
+                    ret = min(ret,
+                              new_match + min_distance_sum(worker_index+1, "".join(bit_mask)))
+                    # backtrack, unmark the bike
+                    bit_mask[bike_index] = "0"
+
+            return ret
+
+        bike_mask = ["0" for i in range(len(bikes))]
+        return min_distance_sum(0, "".join(bike_mask))
