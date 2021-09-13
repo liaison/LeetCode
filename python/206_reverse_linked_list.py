45215a929c66bbdb8fadb6342ea7454e2e5f3129
Lisong <lisong.guo@me.com>
2021-09-13 21:31:43 +0200

    recursion

--- python/206_reverse_linked_list.py
+++ python/206_reverse_linked_list.py
@@ -15,3 +15,24 @@ class Solution:
             curr = next_curr
 
         return prev
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        # return the new head element
+        if head is None or head.next is None:
+            return head
+
+        new_head = self.reverseList(head.next)
+
+        # reverse the pair of elements: head, head->next
+        head.next.next = head
+        head.next = None
+
+        return new_head
