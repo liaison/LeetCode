cf2c47ee9c1b0d8f14e35dfc3e0e96e41c668496
Lisong <lisong.guo@me.com>
2021-09-15 21:02:06 +0200

    comparison of substring for once

--- python/161_one_edit_distance.py
+++ python/161_one_edit_distance.py
@@ -31,3 +31,27 @@ class Solution:
                 is_edited = True
 
         return is_edited
+
+
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+
+        if abs(len(s) - len(t)) > 1:
+            return False
+
+        if len(s) > len(t):
+            long, short = s, t
+        else:
+            long, short = t, s
+
+        NP, NS = len(long), len(short)
+
+        for index in range(NS):
+            if long[index] != short[index]:
+                if NP == NS:
+                    return long[index+1:] == short[index+1:]
+                else: # NP > NS
+                    return long[index+1:] == short[index:]
+
+        return NP == NS + 1
+
