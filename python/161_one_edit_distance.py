839c3fcc28aae354ec3bb1c7778e96c0c6f4880b
Lisong <lisong.guo@me.com>
2021-09-15 21:13:00 +0200

    refined two-pointers solution, less conditions

--- python/161_one_edit_distance.py
+++ python/161_one_edit_distance.py
@@ -34,6 +34,35 @@ class Solution:
 
 
 class Solution:
+    def isOneEditDistanceRefinedTwoPointers(self, s: str, t: str) -> bool:
+
+        if abs(len(s) - len(t)) > 1 or s == t:
+            return False
+
+        ps, pt = 0, 0
+        NS, NT = len(s), len(t)
+        is_edited = False
+
+        while ps < NS and pt < NT:
+
+            if s[ps] != t[pt]:
+                if is_edited:
+                    return False
+
+                if NS > NT:
+                    pt -= 1
+                elif NS < NT:
+                    ps -= 1
+
+                is_edited = True
+
+            ps += 1
+            pt += 1
+
+        return True
+
+
+class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
 
         if abs(len(s) - len(t)) > 1:
