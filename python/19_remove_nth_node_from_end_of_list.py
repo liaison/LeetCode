cb7f86f980dd31c86a0092dccc58048804c3ad50
Lisong <lisong.guo@me.com>
2021-09-16 23:50:50 +0200

    pseudo head

--- python/19_remove_nth_node_from_end_of_list.py
+++ python/19_remove_nth_node_from_end_of_list.py
@@ -25,3 +25,30 @@ class Solution:
         else:
             # remove the head element
             return head.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class SolutionPseudoHead:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        # with a pseudo head, we could have less condition checks
+        pseudo = ListNode(next=head)
+
+        first, second = pseudo, None
+        step = 0
+        while first:
+            if second:
+                second = second.next
+
+            if step == n:
+                second = pseudo
+
+            step += 1
+            first = first.next
+
+        second.next = second.next.next
+
+        return pseudo.next
