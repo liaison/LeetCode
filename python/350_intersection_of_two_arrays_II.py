361aed5d79c74d506cd514bbba24568a14f51096
Lisong <lisong.guo@me.com>
2021-09-17 16:04:07 +0200

    two-pointers

---
+++ python/350_intersection_of_two_arrays_II.py
@@ -0,0 +1,24 @@
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        nums1.sort()
+        nums2.sort()
+
+        p1, p2 = 0, 0
+
+        nums1_len, nums2_len = len(nums1), len(nums2)
+
+        result = []
+        while p1 < nums1_len and p2 < nums2_len:
+            if nums1[p1] == nums2[p2]:
+                result.append(nums1[p1])
+            elif nums1[p1] > nums2[p2]:
+                p1 -= 1
+            else:
+                p2 -= 1
+
+            p1 += 1
+            p2 += 1
+
+        return result
