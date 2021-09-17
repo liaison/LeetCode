393086f7d2f6274ed8d8de74ad6e0a5c96614184
Lisong <lisong.guo@me.com>
2021-09-17 15:47:16 +0200

    DFS

---
+++ python/1448_count_good_nodes_in_binary_tree.py
@@ -0,0 +1,27 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        good_node_cnt = 0
+        def dfs(node, max_value):
+            nonlocal good_node_cnt
+
+            if not node:
+                return
+
+            if node.val >= max_value:
+                good_node_cnt += 1
+
+            new_max_value = max(max_value, node.val)
+            dfs(node.left, new_max_value)
+            dfs(node.right, new_max_value)
+
+        dfs(root, float('-inf'))
+
+        return good_node_cnt
