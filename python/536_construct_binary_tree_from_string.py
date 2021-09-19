11ee885e15e1352fc71e36b96bb092d1988f5977
Lisong <lisong.guo@me.com>
2021-09-19 22:31:14 +0200

    refactoring

--- python/536_construct_binary_tree_from_string.py
+++ python/536_construct_binary_tree_from_string.py
@@ -1,4 +1,3 @@
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,34 +10,37 @@ class Solution:
         if s == "":
             return None
 
-        index = 0
         s_len = len(s)
-        num_str = []
-        while index < s_len:
-            if s[index] != '(':
-                num_str.append(s[index])
-            else:
-                break
-            index += 1
 
-        num = int("".join(num_str))
+        def get_number():
+            index = 0
+            while index < s_len:
+                if s[index] == '(':
+                    break
+                index += 1
+            num = int("".join(s[0:index]))
+            return index, num
+
+        index, num = get_number()
         node = TreeNode(val=num)
 
-        # retrieve the next block of "(substr)"
-        def get_substr(index):
-            substr = []
+        def get_substr(start):
+            """
+                Retrieve a substring in the pattern of "(...)"
+            """
+            index = start
             parenthesis_cnt = 0
             while index < s_len:
                 if s[index] == '(':
                     parenthesis_cnt += 1
                 elif s[index] == ')':
                     parenthesis_cnt -= 1
-                substr.append(s[index])
 
                 if parenthesis_cnt == 0:
                     break
                 index += 1
-            return "".join(substr)
+
+            return s[start:index+1]
 
 
         left_substr = get_substr(index)
@@ -50,4 +52,14 @@ class Solution:
         node.left = left_node
         node.right = right_node
         return node
+
+
+
+
+
+
+
+
+
+
 
