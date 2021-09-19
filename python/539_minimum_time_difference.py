a5f1c6b1bc474622565323df8eed097a5d641d52
Lisong <lisong.guo@me.com>
2021-09-20 00:00:28 +0200

    refactoring

--- python/539_minimum_time_difference.py
+++ python/539_minimum_time_difference.py
@@ -1,17 +1,13 @@
-
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
 
         def to_minutes(time_str):
-            hh = int(time_str[0:2])
-            mm = int(time_str[3:])
-            return hh * 60 + mm
+            return int(time_str[0:2]) * 60 + int(time_str[3:])
 
-        timestamps = []
-        for time_str in timePoints:
-            timestamps.append(to_minutes(time_str))
+        timestamps = [to_minutes(time_str) for time_str in timePoints]
 
         timestamps.sort()
+
         min_gap = float('inf')
         for index in range(1, len(timestamps)):
             min_gap = min(min_gap, timestamps[index] - timestamps[index-1])
