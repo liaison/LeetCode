79c2882d965ffcbef58b563322f5a541c6147bb4
Lisong <lisong.guo@me.com>
2021-09-20 21:10:54 +0200

    two-pointers approach

--- python/11_container_with_most_water.py
+++ python/11_container_with_most_water.py
@@ -11,3 +11,20 @@ class SolutionTLE:
                 max_size = max(max_size, new_size)
 
         return max_size
+
+
+class SolutionTwoPointer:
+    def maxArea(self, height: List[int]) -> int:
+
+        left, right = 0, len(height) - 1
+        max_size = float('-inf')
+        while left < right:
+            new_size = min(height[left], height[right]) * (right - left)
+            max_size = max(max_size, new_size)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_size
