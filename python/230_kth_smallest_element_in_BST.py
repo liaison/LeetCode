415dcb9f124066144dd9be207250b2c9b1ee8770
Lisong <lisong.guo@me.com>
2021-09-20 16:47:16 +0200

    Iterative inorder traversal

--- python/230_kth_smallest_element_in_BST.py
+++ python/230_kth_smallest_element_in_BST.py
@@ -62,3 +62,31 @@ class SolutionConstantSpace:
         return index_num[1]
 
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        stack = []
+        curr_node = root
+
+        while True:
+            # left first
+            while curr_node:
+                stack.append(curr_node)
+                curr_node = curr_node.left
+
+            curr_node = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr_node.val
+
+            # move on to the right child
+            curr_node = curr_node.right
+
+
