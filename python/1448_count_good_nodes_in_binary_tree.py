30233b1e1cdca48a2520da90ebc2192b712d91cf
Lisong <lisong.guo@me.com>
2021-09-20 16:03:02 +0200

    implement DFS with stack

--- python/1448_count_good_nodes_in_binary_tree.py
+++ python/1448_count_good_nodes_in_binary_tree.py
@@ -25,3 +25,32 @@ class Solution:
         dfs(root, float('-inf'))
 
         return good_node_cnt
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class SolutionDFSwithStack:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        good_nodes_cnt = 0
+
+        stack = [(root, float('-inf'))]
+
+        while stack:
+            curr, max_sofar = stack.pop()
+
+            if max_sofar <= curr.val:
+                good_nodes_cnt += 1
+                max_sofar = curr.val
+
+            for next_node in [curr.left, curr.right]:
+                if next_node:
+                    stack.append((next_node, max_sofar))
+
+        return good_nodes_cnt
+
+
