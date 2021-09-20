2634517f21546d0b9fd91328f0f5d0cdb8e05a93
Lisong <lisong.guo@me.com>
2021-09-20 16:40:50 +0200

    DFS with less space

--- python/230_kth_smallest_element_in_BST.py
+++ python/230_kth_smallest_element_in_BST.py
@@ -27,3 +27,38 @@ class Solution:
         return ordered_list[k-1]
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class SolutionConstantSpace:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        index_num = (-1, 0)
+
+        def inorder_traverse(node):
+            nonlocal index_num
+
+            if not node:
+                return
+
+            # Early interrupt for the traversal
+            index, num = index_num
+            if index == k-1:
+                return
+
+            inorder_traverse(node.left)
+
+            index, num = index_num
+            if index < k-1:
+                index_num = index+1, node.val
+
+            inorder_traverse(node.right)
+
+        inorder_traverse(root)
+
+        return index_num[1]
+
+
