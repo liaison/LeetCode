1ce6438c2334603ae54e0dec60b0c6faa2268215
Lisong <lisong.guo@me.com>
2021-09-20 17:45:11 +0200

    traverse the BST in binary search order

--- python/270_closest_binary_search_tree_value.py
+++ python/270_closest_binary_search_tree_value.py
@@ -16,8 +16,35 @@ class Solution(object):
         #   check every node that we traverse.
         while root:
             closest_node = min(root, closest_node, key = lambda x: abs(target - x.val))
-            
+
             # next iteration
             root = root.left if target < root.val else root.right
-        
+
+        return closest_node.val
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+
+        curr = root
+        closest_node = root
+        min_value = float('inf')
+
+        while curr:
+            diff = abs(curr.val - target)
+            if diff < min_value:
+                min_value = diff
+                closest_node = curr
+
+            if curr.val < target:
+                curr = curr.right
+            else:
+                curr = curr.left
+
         return closest_node.val
