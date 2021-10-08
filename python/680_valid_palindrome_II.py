7d38c562af790cec2d0d8031699fc1bef07c190b
Lisong <lisong.guo@me.com>
2021-10-09 00:40:45 +0200

    Greedy

--- python/680_valid_palindrome_II.py
+++ python/680_valid_palindrome_II.py
@@ -40,3 +40,18 @@ class SolutionDFS:
 
         return left >= right
 
+
+class SolutionGreedy:
+    def validPalindrome(self, s: str) -> bool:
+
+        left, right = 0, len(s)-1
+
+        while s[left] == s[right] and left < right:
+            left += 1
+            right -= 1
+
+        delete_left = s[left+1:right+1]
+        delete_right = s[left:right]
+
+        return delete_left == delete_left[::-1] or delete_right == delete_right[::-1]
+
