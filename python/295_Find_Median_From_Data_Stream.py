b48f4e7174d276f017d9803882573afe1d8e2a9a
Lisong <lisong.guo@me.com>
2021-10-20 23:48:24 +0200

    solution with two heaps. pushing through both low and high heaps

--- python/295_Find_Median_From_Data_Stream.py
+++ python/295_Find_Median_From_Data_Stream.py
@@ -22,3 +22,40 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+import heapq
+
+class MedianFinderHeap:
+    """
+        Solution with two heaps
+    """
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.low = []
+        self.high = []
+
+    def addNum(self, num: int) -> None:
+        # push the element through both low and high heap
+        heapq.heappush(self.low, - num)
+        heapq.heappush(self.high, - self.low[0])
+        heapq.heappop(self.low)
+
+        # rebalancing, to ensure len(low) >= len(high)
+        if len(self.low) < len(self.high):
+            heapq.heappush(self.low, - self.high[0])
+            heapq.heappop(self.high)
+
+
+    def findMedian(self) -> float:
+        if len(self.low) > len(self.high):
+            return - self.low[0]
+        else:
+            return (- self.low[0] + self.high[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
