3ab19542e7145d2e14be8adc714a53323c976651
Lisong <lisong.guo@me.com>
2021-10-28 23:45:56 +0200

    using built-in function to do the conversion between string and integer

--- python/670_maximum_swap.py
+++ python/670_maximum_swap.py
@@ -39,3 +39,33 @@ class Solution:
             power *= 10
 
         return final_num
+
+
+
+class SolutionBuiltIn:
+    def maximumSwap(self, num: int) -> int:
+        # convert integer to a list of digits
+        digits = list(str(num))
+
+        digits_len = len(digits)
+        def swap(curr):
+            if curr == digits_len - 1:
+                return
+
+            max_index = curr
+            curr_max = digits[curr]
+            for index in range(curr+1, digits_len):
+                if curr_max <= digits[index]:
+                    max_index = index
+                    curr_max = digits[index]
+
+            if max_index == curr or digits[max_index] == digits[curr]:
+                swap(curr+1)
+            else:
+                digits[curr], digits[max_index] = digits[max_index], digits[curr]
+
+        # greedily make a swap
+        swap(0)
+
+        # convert the list of digits back to an integer
+        return int("".join(digits))
