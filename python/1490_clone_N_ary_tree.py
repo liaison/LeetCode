6b65d19155805d3ce890e892d8be56f14b17c895
Lisong <lisong.guo@me.com>
2022-01-17 00:06:24 +0100

    DFS with stack

--- python/1490_clone_N_ary_tree.py
+++ python/1490_clone_N_ary_tree.py
@@ -17,3 +17,32 @@ class Solution:
             node_copy.children.append(self.cloneTree(child))
 
         return node_copy
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class SolutionDFS:
+    def cloneTree(self, root: 'Node') -> 'Node':
+
+        if not root:
+            return root
+
+        new_root = Node(root.val)
+
+        # DFS/BFS
+        stack = [(root, new_root)]
+        while stack:
+            old_node, new_node = stack.pop()
+            for child_node in old_node.children:
+                new_child_node = Node(child_node.val)
+                new_node.children.append(new_child_node)
+                # 'recursively' make deep copy for each child node
+                stack.append((child_node, new_child_node))
+
+        return new_root
