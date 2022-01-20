c8bc546467246fc4e090b2aea1e0ca9369cd4467
Lisong <lisong.guo@me.com>
2022-01-20 23:02:46 +0100

    BFS

--- python/1490_clone_N_ary_tree.py
+++ python/1490_clone_N_ary_tree.py
@@ -46,3 +46,30 @@ class SolutionDFS:
                 stack.append((child_node, new_child_node))
 
         return new_root
+
+
+
+class SolutionBFS:
+    def cloneTree(self, root: 'Node') -> 'Node':
+
+        if not root:
+            return root
+
+        new_root = Node(root.val)
+        # Starting point to kick off the BFS visits.
+        queue = deque([(root, new_root)])
+
+        while queue:
+            # Get the element from the head of the queue.
+            old_node, new_node = queue.popleft()
+
+            for child_node in old_node.children:
+                new_child_node = Node(child_node.val)
+
+                # Make a copy for each child node.
+                new_node.children.append(new_child_node)
+
+                # Schedule a visit to copy the child nodes of each child node.
+                queue.append((child_node, new_child_node))
+
+        return new_root
