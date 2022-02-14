1e81076068a46bc8c683908890c7a1c0a53bf19f
Lisong <lisong.guo@me.com>
2022-02-14 20:56:28 +0100

    using regex to split string

--- python/1087_brace_expansion.py
+++ python/1087_brace_expansion.py
@@ -1,31 +1,56 @@
 class SolutionStrManipulation:
     def expand(self, S: str) -> List[str]:
-        
+
         blocks = S.replace("{", " ").replace("}", " ").strip().split()
         dict_list = [sorted(block.split(',')) for block in blocks]
-        
+
         results = []
-        
+
         def backtrack(curr, comb):
             if curr == len(dict_list):
                 results.append("".join(comb))
                 return
-            
+
             for letter in dict_list[curr]:
                 comb[curr] = letter
                 backtrack(curr+1, comb)
-        
+
         comb = [""] * len(dict_list)
         backtrack(0, comb)
-        
+
         return results
+
+class SolutionRegex:
+    def expand(self, s: str) -> List[str]:
+
+        import re
+        str_list = re.split("[\{\}]", s)
+        levels = [sorted(string.split(',')) for string in str_list]
+
+        output = []
+        end_level = len(levels)
+        def dfs(curr_level, comb):
+            nonlocal output
+
+            if curr_level == end_level:
+                output.append("".join(comb))
+                return
+
+            for candidate in levels[curr_level]:
+                comb.append(candidate)
+                dfs(curr_level + 1, comb)
+                comb.pop()
+
+        dfs(0, [])
+
+        return output
 
 
 class Solution:
     def expand(self, S: str) -> List[str]:
-        
+
         dict_list = []
-        
+
         curr = 0
         while curr < len(S):
             if S[curr] == "{":
@@ -38,19 +63,19 @@ class Solution:
             else:
                 dict_list.append([S[curr]])
             curr += 1
-        
+
         results = []
-        
+
         def backtrack(curr, comb):
             if curr == len(dict_list):
                 results.append("".join(comb))
                 return
-            
+
             for letter in dict_list[curr]:
                 comb[curr] = letter
                 backtrack(curr+1, comb)
-        
+
         comb = [""] * len(dict_list)
         backtrack(0, comb)
-        
+
         return results
