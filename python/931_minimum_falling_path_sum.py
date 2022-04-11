a3962e66f63bdd08ca82a3a55193b9fc1afe69bd
Lisong <lisong.guo@me.com>
2022-04-11 21:37:40 +0200

    dynamic programming with more efficient memory management

--- python/931_minimum_falling_path_sum.py
+++ python/931_minimum_falling_path_sum.py
@@ -29,3 +29,25 @@ class Solution:
             result = min(result, min_path(0, col))
 
         return result
+
+
+class SolutionDPMemoryEfficient:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+
+        max_row, max_col = len(matrix) - 1, len(matrix[0]) - 1
+
+        prev_min_path = matrix[max_row]
+        next_min_path = [float('inf')] * (max_col+1)
+
+        for row_index in range(max_row-1, -1, -1):
+            # get the min_path_sum from the current cell
+            for col_index in range(0, max_col+1):
+                for prev_col in [col_index-1, col_index, col_index+1]:
+                    if 0 <= prev_col and prev_col <= max_col:
+                        next_min_path[col_index] = min(next_min_path[col_index], prev_min_path[prev_col])
+                next_min_path[col_index] += matrix[row_index][col_index]
+
+            prev_min_path = next_min_path
+            next_min_path = [float('inf')] * (max_col+1)
+
+        return min(prev_min_path)
