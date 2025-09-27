2c59d33b34bc86635839c8eeaa60f4677f6ec762
Lisong <lisong.guo@me.com>
2025-09-28 01:11:46 +0200

    using vector to avoid division-by-zero

--- python/1232_check_straight_line.py
+++ python/1232_check_straight_line.py
@@ -20,3 +20,33 @@ class Solution:
                 return False
 
         return True
+
+
+    def checkStraightLine_vector(self, coordinates: List[List[int]]) -> bool:
+        """
+            Using the vector representation, to avoid the division-by-zero
+
+            x_1/y_1 == x_2/y_2 ---> (x_1 * y_2) == (x_2 * y_1)
+
+        """
+        def angle_vector(start, end):
+            start_x, start_y = start[0], start[1]
+            end_x, end_y = end[0], end[1]
+            x_offset = end_x - start_x
+            y_offset = end_y - start_y
+            return (x_offset, y_offset)
+
+        def is_equal_vector(vec_one, vec_other):
+            x_one, y_one = vec_one
+            x_other, y_other = vec_other
+            return (x_one * y_other) == (x_other * y_one)
+
+        start_angle = angle_vector(coordinates[0], coordinates[1])
+        start_point = coordinates[0]
+        for index in range(2, len(coordinates)):
+            next_angle = angle_vector(coordinates[index], start_point)
+
+            if not is_equal_vector(start_angle, next_angle):
+                return False
+
+        return True
