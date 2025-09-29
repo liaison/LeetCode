14eef4e71a6765e619cd49485ea8f367c511a0b5
Lisong <lisong.guo@me.com>
2025-09-29 16:27:41 +0200

    recursion in a pythonic way

--- python/559_maximum_depth_N-ary_tree.py
+++ python/559_maximum_depth_N-ary_tree.py
@@ -8,21 +8,45 @@ class Node:
 
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
-        
+
         max_depth = 0
-        
+
         def dfs(node, curr_depth):
             nonlocal max_depth
-            
+
             if node is None:
                 return
-            
+
             if len(node.children) == 0:
                 max_depth = max(max_depth, curr_depth)
             else:
                 for child in node.children:
                     dfs(child, curr_depth+1)
-    
+
         dfs(root, 1)
-        
+
         return max_depth
+
+
+    def maxDepth_rec(self, root: 'Node') -> int:
+
+        if not root:
+            return 0
+
+        max_depth = 1
+        for child in root.children:
+            max_depth = max(max_depth, self.maxDepth_rec(child) + 1)
+
+        return max_depth
+
+
+    def maxDepth_py(self, root: 'Node') -> int:
+
+        if not root:
+            return 0
+
+        # avoid max([]) error
+        if len(root.children) == 0:
+            return 1
+
+        return max([self.maxDepth_py(child) for child in root.children]) + 1
