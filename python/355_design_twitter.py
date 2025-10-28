7d98950decff298e2194796ddadfe2c277674427
Lisong <lisong.guo@me.com>
2025-10-28 23:59:37 +0100

    heap

--- python/355_design_twitter.py
+++ python/355_design_twitter.py
@@ -18,10 +18,60 @@ class Twitter:
         merged_tweets = self.user_to_tweets_map[userId][-10:]
         for followee in self.follower_map[userId]:
             followee_tweets = self.user_to_tweets_map[followee]
-            merged_tweets.extend(followee_tweets)
+            merged_tweets.extend(followee_tweets[-10:])
 
         merged_tweets.sort(reverse = True)
         return [tweetId for timestamp, tweetId in merged_tweets[:10]]
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follower_map[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.follower_map[followerId]:
+            self.follower_map[followerId].remove(followeeId)
+
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
+
+
+class TwitterHeap:
+
+    def __init__(self):
+        # user -> [(timestamp, tweetId)]
+        self.user_to_tweets_map = defaultdict(list)
+
+        # follower -> set of followees
+        self.follower_map = defaultdict(set)
+        self._timestamp = 1
+
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.user_to_tweets_map[userId].append((-self._timestamp, tweetId))
+        self._timestamp += 1
+
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        merged_tweets = self.user_to_tweets_map[userId][-10:]
+        for followee in self.follower_map[userId]:
+            followee_tweets = self.user_to_tweets_map[followee]
+            merged_tweets.extend(followee_tweets[-10:])
+
+        heapq.heapify(merged_tweets)
+        top_tweets = []
+        index = 0
+        total_length = len(merged_tweets)
+        while index < 10 and index < total_length:
+            index += 1
+            timestamp, most_recent_tweet = heapq.heappop(merged_tweets)
+            top_tweets.append(most_recent_tweet)
+        return top_tweets
 
 
     def follow(self, followerId: int, followeeId: int) -> None:
